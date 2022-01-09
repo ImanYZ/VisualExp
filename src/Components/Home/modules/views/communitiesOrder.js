3a3a18b27b5328f9a4ad6aa7df3434915beecc36
Iman <oneweb@umich.edu>
2022-01-09 00:46:48 -0500

    Added the community images.

---
+++ src/Components/Home/modules/views/communitiesOrder.js
@@ -0,0 +1,47 @@
+export default [
+  {
+    url: "/static/Communities/Educational_Organizational_Psychology.jpg",
+    title: "Educational/Organizational Psychology",
+    width: "40%",
+  },
+  {
+    url: "/static/Communities/Social_Political_Psychology.jpg",
+    title: "Social/Political Psychology",
+    width: "30%",
+  },
+  {
+    url: "/static/Communities/Deep_Learning.jpg",
+    title: "Deep Learning",
+    width: "30%",
+  },
+  {
+    url: "/static/Communities/Health_Psychology.png",
+    title: "Health Psychology",
+    width: "25%",
+  },
+  {
+    url: "/static/Communities/Neuroscience.jpg",
+    title: "Neuroscience",
+    width: "40%",
+  },
+  {
+    url: "/static/LibraryBackground.jpg",
+    title: "Librarians",
+    width: "35%",
+  },
+  {
+    url: "/static/Communities/Mindfulness.jpg",
+    title: "Mindfulness",
+    width: "40%",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1533727937480-da3a97967e95?auto=format&fit=crop&w=400&q=80",
+    title: "Fitness",
+    width: "20%",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1518136247453-74e7b5265980?auto=format&fit=crop&w=400&q=80",
+    title: "Reading",
+    width: "40%",
+  },
+];
