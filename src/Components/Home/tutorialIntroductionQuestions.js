9d6cc26b0351d1f43c2b44285568605bcd735944
Iman Yeckehzaare <oneweb@m-c02m21erf8jc.attlocal.net>
2022-01-22 11:00:09 -0500

    Adding tutorial introduction questions.

---
+++ src/Components/Home/tutorialIntroductionQuestions.js
@@ -0,0 +1,11 @@
+export default [
+  {
+    stem: "How can 1Cademy help us?",
+    a: "Through learning backwards like researchers",
+    b: "Joining/forming multi-school research communities",
+    c: "Passing standardized exams",
+    d: "Deepening our learning by thinking through prerequisites necessary to learn each concept",
+    e: "Better scheduling our studying sessions",
+    answers: [a, b, d],
+  },
+];
