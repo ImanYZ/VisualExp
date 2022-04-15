1914b4544524c8a1497fb009672ab74235a740a1
Iman Yeckehzaare <oneweb@m-c02m21erf8jc.attlocal.net>
2022-04-15 10:51:02 -0400

    Fixed the issue with dataGrid visibility in MangeEvents.js.

--- src/Components/ProjectManagement/ManageEvents/ManageEvents.js
+++ src/Components/ProjectManagement/ManageEvents/ManageEvents.js
@@ -915,7 +915,7 @@ const ManageEvents = (props) => {
           onRowClick={gridRowClick}
         />
         {schedule.length > 0 && (
-          <div style={{ marginBottom: "280px" }}>
+          <div style={{ marginBottom: "700px" }}>
             <SelectSessions
               startDate={new Date()}
               numDays={16}
