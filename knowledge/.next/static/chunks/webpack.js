0b82fd668518922ca6f1ec5067afbf6e6e981cc2
Iman Yeckehzaare <oneweb@m-c02m21erf8jc.attlocal.net>
2022-05-10 10:26:43 -0400

    Fixed the issue with FIREBASE AUTHENTICATION ON KNOWLEDGE CLOUD RUN.

--- knowledge/.next/static/chunks/webpack.js
+++ knowledge/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9864a14672f9b939"; }
+/******/ 		__webpack_require__.h = function() { return "1bcc52911bcb4825"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
