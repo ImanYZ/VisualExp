f229f96ee9b3a5ee98da8d63ba9070b277064035
Iman Yeckehzaare <oneweb@m-c02m21erf8jc.attlocal.net>
2022-05-12 21:03:54 -0400

    Adding the AppAppBar.js to knowledge.

--- knowledge/lib/nodes.js
+++ knowledge/lib/nodes.js
@@ -1,5 +1,6 @@
 import { remark } from "remark";
 import html from "remark-html";
+import geoip from "geoip-lite";
 
 import {
   admin,
@@ -164,4 +165,28 @@ export const getNodeData = async (id) => {
     children,
     parents,
   };
+};
+
+// Get the user's useragent and location and log views in the database.
+export const logViews = async (req) => {
+  // let userAgent;
+  // if (req) {
+  //   // if you are on the server and you get a 'req' property from your context
+  //   userAgent = req.headers["user-agent"]; // get the user-agent from the headers
+  // } else {
+  //   userAgent = navigator.userAgent; // if you are on the client you can access the navigator from the window object
+  // }
+  // const forwarded = req.headers["x-forwarded-for"];
+  // const ip = forwarded
+  //   ? forwarded.split(/, /)[0]
+  //   : req.connection.remoteAddress;
+  // const geo = geoip.lookup(ip);
+  // const viewerRef = db.collection("viewers").doc();
+  // await viewerRef.update({
+  //   ip,
+  //   userAgent,
+  //   country: geo.country,
+  //   region: geo.region,
+  //   city: geo.city,
+  // });
 };
