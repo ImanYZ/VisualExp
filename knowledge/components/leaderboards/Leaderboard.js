7790074a795adc9b3444519c93b00557d8f36213
Iman Yeckehzaare <oneweb@m-c02m21erf8jc.attlocal.net>
2022-05-17 18:20:09 -0400

    Added institutions leaderboard for each node page and grouped the instituions and contributors' components into Leaderboard.

---
+++ knowledge/components/leaderboards/Leaderboard.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+import Box from "@mui/material/Box";
+
+import LeaderboardChip from "./LeaderboardChip";
+
+const Leaderboard = ({ data, objType }) => {
+  return (
+    <>
+      <Box component="h4">{objType}</Box>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "left",
+          flexWrap: "wrap",
+          listStyle: "none",
+          p: 0.5,
+          m: 0,
+        }}
+        component="ul"
+      >
+        {data &&
+          data.map((obj, idx) => {
+            const name =
+              objType === "Institutions"
+                ? obj.name
+                : obj.chooseUname
+                ? obj.username
+                : obj.fullname;
+            const imageUrl =
+              objType === "Institutions" ? obj.logoURL : obj.imageUrl;
+            console.log({ imageUrl });
+            return (
+              <LeaderboardChip
+                key={obj.name + idx}
+                idx={idx}
+                objType={objType}
+                name={name}
+                imageUrl={imageUrl}
+                reputation={obj.reputation}
+              />
+            );
+          })}
+      </Box>
+    </>
+  );
+};
+
+export default Leaderboard;
