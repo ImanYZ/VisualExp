aa6c3b41016c0016884f63c625d645b18c124944
Xavier Perez <xap5xap@users.noreply.github.com>
2022-05-19 18:11:51 -0500

    Fix app bar

--- knowledge/components/AppHeaderNavbar.tsx
+++ knowledge/components/AppHeaderNavbar.tsx
@@ -1,164 +1,61 @@
-import Head from "next/head";
 import Image from "next/image";
-
-import React, { useState, useEffect } from "react";
-
-// import IconButton from "@mui/material/IconButton";
-import Box from "@mui/material/Box";
+import NextLink from "next/link";
+import ROUTES from "../src/routes";
+import React, { FC } from "react";
 import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
 import Link from "@mui/material/Link";
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
-import Tooltip from "@mui/material/Tooltip";
-import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
-
-import LogoutIcon from "@mui/icons-material/Logout";
-import AccountCircle from "@mui/icons-material/AccountCircle";
-// import Brightness4Icon from "@mui/icons-material/Brightness4";
-// import Brightness7Icon from "@mui/icons-material/Brightness7";
-
-import AppBar from "./AppBar";
-import Toolbar from "./Toolbar";
-
-import sectionsOrder from "./sectionsOrder";
 
 import LogoDarkMode from "../public/DarkModeLogo.svg";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import SECTIONS from "../src/navbarSections";
+import Tooltip from "@mui/material/Tooltip";
 
-export const siteTitle = "1Cademy Knowledge Graph!";
-
-const LinkTab = (props) => {
-  return (
-    <Tooltip title={props.titl}>
-      <Tab
-        onClick={(event) => {
-          event.preventDefault();
-          props.onClick(event);
-        }}
-        color="inherit"
-        {...props}
-      />
-    </Tooltip>
-  );
+type Props = {
+  showApply?: boolean;
 };
-
-const AppAppBar = (props) => {
+const AppAppBar: FC<Props> = ({ showApply = true }) => {
   return (
-    <div>
-      <Head>
-        <link rel="icon" href="/public/favicon.ico" />
-        <meta
-          name="description"
-          content="1Cademy Knowledge Graph Public Interface!"
-        />
-        <meta property="og:image" content="/public/LogoThumbnail.png" />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Head>
-      <AppBar>
-        <Toolbar sx={{ justifyContent: "space-between" }}>
-          <Tooltip title="1Cademy's Landing Page">
-            <Box
-              variant="h6"
-              underline="none"
-              color="inherit"
-              onClick={props.homeClick}
-              sx={{
-                fontSize: 24,
-                margin: "7px 19px 0px -10px",
-                cursor: "pointer",
-              }}
-            >
-              <Image src={LogoDarkMode} alt="logo" width="52px" />
-            </Box>
-          </Tooltip>
-          <Tabs
-            value={props.section}
-            variant="scrollable"
-            scrollButtons="auto"
-            allowScrollButtonsMobile
-            aria-label="scrollable auto tabs navigation bar"
-            sx={{
-              marginLeft: "auto",
-              fontWeight: 400,
-              "& .MuiTab-root": {
-                color: "#AAAAAA",
-              },
-              "& .MuiTab-root.Mui-selected": {
-                color: "common.white",
-              },
-              "& .MuiTabs-indicator": {
-                backgroundColor: "secondary.main",
-              },
-            }}
-          >
-            {[0, 1, 2, 3, 4].map((idx) => {
-              return (
-                <LinkTab
-                  key={"Key" + idx}
-                  onClick={props.switchSection(idx)}
-                  label={sectionsOrder[idx + 1].label}
-                  titl={sectionsOrder[idx + 1].title}
-                />
-              );
-            })}
-            {props.thisPage && (
-              <LinkTab
-                onClick={props.switchSection(5)}
-                label={props.thisPage}
-                titl={props.thisPage}
-              />
-            )}
-          </Tabs>
-          <Box sx={{ flex: 1, display: "flex", justifyContent: "flex-end" }}>
-            {/* <Box
-              sx={{
-                display: "flex",
-                width: "100%",
-                alignItems: "center",
-                justifyContent: "center",
-                bgcolor: "background.default",
-                color: "text.primary",
-                borderRadius: 1,
-                p: 3,
-              }}
-            >
-              {colorMode} mode
-              <IconButton
-                sx={{ ml: 1 }}
-                onClick={toggleColorMode}
-                color="inherit"
-              >
-                {colorMode === "dark" ? (
-                  <Brightness7Icon />
-                ) : (
-                  <Brightness4Icon />
-                )}
-              </IconButton>
-            </Box> */}
-            {!props.tutorial && !props.joinNowSec && !props.communities && (
-              <Tooltip title="Apply to join 1Cademy">
+    <AppBar>
+      <Toolbar sx={{ justifyContent: "space-between" }}>
+        <Box sx={{ my: 1 }}>
+          <NextLink href={ROUTES.home} passHref prefetch={false}>
+            <Link sx={{ display: "flex" }}>
+              <Image
+                src={LogoDarkMode}
+                alt="logo"
+                width="60px"
+                height="60px"
+              ></Image>
+            </Link>
+          </NextLink>
+        </Box>
+        <Box sx={{ flexGrow: 1, ml: 2, display: { xs: "none", md: "flex" } }}>
+          {SECTIONS.map((page, idx) => (
+            <NextLink key={idx} href={page.route} passHref>
+              <Tooltip title={page.title} arrow>
                 <Button
-                  variant="contained"
-                  color="secondary"
-                  onClick={props.joinUsClick}
-                  sx={{
-                    fontSize: 16,
-                    color: "common.white",
-                    ml: 2.5,
-                    borderRadius: 40,
-                  }}
+                  size="large"
+                  sx={{ my: 2, color: "white", display: "block" }}
                 >
-                  Apply!
+                  {page.label}
                 </Button>
               </Tooltip>
-            )}
-          </Box>
-        </Toolbar>
-      </AppBar>
-      <Toolbar />
-    </div>
+            </NextLink>
+          ))}
+        </Box>
+        <Box sx={{ flex: 1, display: "flex", justifyContent: "flex-end" }}>
+          {showApply && (
+            <Tooltip title="Apply to join 1Cademy">
+              <Button size="large" variant="contained">
+                Apply!
+              </Button>
+            </Tooltip>
+          )}
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 };
 
---
+++ knowledge/src/navbarSections.ts
@@ -0,0 +1,34 @@
+const SECTIONS = [
+  {
+    id: "HowItWorksSection",
+    label: "How",
+    title: "How We Work",
+    route: "/changeThis",
+  },
+  {
+    id: "CommunitiesSection",
+    label: "What",
+    title: "Our Communities",
+    route: "/changeThis",
+  },
+  {
+    id: "ValuesSection",
+    label: "Why",
+    title: "Why 1Cademy Helps",
+    route: "/changeThis",
+  },
+  {
+    id: "SchoolsSection",
+    label: "Where",
+    title: "Where Are We From?",
+    route: "/changeThis",
+  },
+  {
+    id: "WhoWeAreSection",
+    label: "Who",
+    title: "Who Is Behind 1Cademy?",
+    route: "/changeThis",
+  },
+];
+
+export default SECTIONS;
