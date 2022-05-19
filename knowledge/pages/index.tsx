d9dc2b1f1eab5e4ddc9fb7f4aea04b621f069ada
Xavier Perez <xap5xap@users.noreply.github.com>
2022-05-19 18:11:51 -0500

    Fix app bar

--- knowledge/pages/index.tsx
+++ knowledge/pages/index.tsx
@@ -12,20 +12,8 @@ import { Button } from "@mui/material";
 type Props = {
   data: KnowledgeNode[];
 };
-// This value is considered fresh for ten seconds (s-maxage=10).
-// If a request is repeated within the next 10 seconds, the previously
-// cached value will still be fresh. If the request is repeated before 59 seconds,
-// the cached value will be stale but still render (stale-while-revalidate=59).
-//
-// In the background, a revalidation request will be made to populate the cache
-// with a fresh value. If you refresh the page, you will see the new value.
-export const getServerSideProps: GetServerSideProps<Props> = async ({
-  res,
-}) => {
-  res.setHeader(
-    "Cache-Control",
-    "public, s-maxage=10, stale-while-revalidate=59"
-  );
+
+export const getServerSideProps: GetServerSideProps<Props> = async ({}) => {
   const allPostsData = await getSortedPostsData();
   return {
     props: {
@@ -44,7 +32,7 @@ const Home: NextPage<Props> = ({ data }) => {
   };
 
   return (
-    <PagesNavbar thisPage="Node">
+    <PagesNavbar>
       <Container>
         <Box sx={{ mb: 2, display: "flex", flexDirection: "row" }}>
           <SearchInput onSearch={handleSearch}></SearchInput>
