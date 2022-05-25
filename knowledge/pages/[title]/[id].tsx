3d08630f46ec2ccc64b15193a4de9aa2b7c4e0d2
Xavier Perez <xap5xap@users.noreply.github.com>
2022-05-25 13:35:53 -0500

    Migrate leaderboard chip

--- knowledge/pages/[title]/[id].tsx
+++ knowledge/pages/[title]/[id].tsx
@@ -9,6 +9,7 @@ import NodeHead from "../../components/NodeHead";
 import Grid from "@mui/material/Grid";
 import LinkedNodes from "../../components/LinkedNodes";
 import NodeItem from "../../components/NodeItem";
+import NodeItemContributors from "../../components/NodeItemContributors";
 
 type Props = {
   node: KnowledgeNode;
@@ -21,10 +22,7 @@ interface Params extends ParsedUrlQuery {
   id: string;
 }
 
-export const getServerSideProps: GetServerSideProps<Props, Params> = async ({
-  req,
-  params,
-}) => {
+export const getServerSideProps: GetServerSideProps<Props, Params> = async ({ req, params }) => {
   logViews(req, params?.id || "");
   const nodeData = await getNodeData(params?.id || "");
   if (!nodeData) {
@@ -38,12 +36,8 @@ export const getServerSideProps: GetServerSideProps<Props, Params> = async ({
     keywords += escapeBreaksQuotes(tag.title) + ", ";
   }
 
-  const updatedStr = nodeData.changedAt
-    ? dayjs(new Date(nodeData.changedAt)).format("YYYY-MM-DD")
-    : "";
-  const createdStr = nodeData.createdAt
-    ? dayjs(new Date(nodeData.createdAt)).format("YYYY-MM-DD")
-    : "";
+  const updatedStr = nodeData.changedAt ? dayjs(new Date(nodeData.changedAt)).format("YYYY-MM-DD") : "";
+  const createdStr = nodeData.createdAt ? dayjs(new Date(nodeData.createdAt)).format("YYYY-MM-DD") : "";
 
   return {
     props: {
@@ -55,26 +49,21 @@ export const getServerSideProps: GetServerSideProps<Props, Params> = async ({
   };
 };
 
-const NodePage: NextPage<Props> = ({
-  node,
-  keywords,
-  createdStr,
-  updatedStr,
-}) => {
+const NodePage: NextPage<Props> = ({ node, keywords, createdStr, updatedStr }) => {
   return (
     <PagesNavbar title={`1Cademy - ${node.title}`}>
-      <NodeHead
-        node={node}
-        keywords={keywords}
-        createdStr={createdStr}
-        updatedStr={updatedStr}
-      />
+      <NodeHead node={node} keywords={keywords} createdStr={createdStr} updatedStr={updatedStr} />
       <Grid container spacing={3}>
         <Grid item xs={12} sm={12} md={3}>
           <LinkedNodes data={node.parents || []} header="Learn Before" />
         </Grid>
         <Grid item xs={12} sm={12} md={6}>
-          <NodeItem node={node} />
+          <NodeItem
+            node={node}
+            contributors={
+              <NodeItemContributors contributors={node.contributors || []} institutions={node.institutions || []} />
+            }
+          />
         </Grid>
         <Grid item xs={12} sm={12} md={3}>
           <LinkedNodes data={node.children || []} header="Learn After" />
