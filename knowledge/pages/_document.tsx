0340fdf2dbe2b03614170e908b0d423d62d4ca98
jimy-nicanor <dev.jimynicanor@gmail.com>
2022-05-26 18:38:36 -0500

    finish change hs to ts

--- knowledge/pages/_document.tsx
+++ knowledge/pages/_document.tsx
@@ -1,104 +1,92 @@
-import Document, { DocumentContext } from "next/document";
+import createEmotionServer from "@emotion/server/create-instance";
+import Document, { DocumentContext, Head, Html, Main, NextScript } from "next/document";
+import * as React from "react";
 
-class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx);
+import { getMetaThemeColor } from "../src/brandingTheme";
+import createEmotionCache from "../src/createEmotionCache";
 
-    return initialProps;
+export default class MyDocument extends Document<{ emotionStyleTags: any }> {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta name="theme-color" content={getMetaThemeColor("light")} media="(prefers-color-scheme: light)" />
+          <meta name="theme-color" content={getMetaThemeColor("dark")} media="(prefers-color-scheme: dark)" />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <meta name="google-site-verification" content="XpDB7dCQFlAHNdpUyVmw5Iahh4FzqPzUMUmlOLoNs-Y" />
+          <meta charSet="UTF-8" />
+          <meta name="robots" content="index, follow" />
+          <meta name="googlebot" content="index, follow" />
+          <meta name="language" content="ENG" />
+          <meta name="copyright" content="1Cademy" />
+          <meta name="reply-to" content="onecademy@umich.edu" />
+          <meta name="coverage" content="Worldwide" />
+          <meta name="distribution" content="Global" />
+          <meta name="rating" content="General" />
+          <meta name="target" content="all" />
+          <meta name="HandheldFriendly" content="False" />
+          <meta name="author" content="1Cademy" />
+          <meta httpEquiv="Expires" content="-1" />
+          <meta httpEquiv="Pragma" content="no-cache" />
+          <meta httpEquiv="Cache-Control" content="no-cache" />
+          <meta httpEquiv="x-dns-prefetch-control" content="off" />
+          {/* PWA primary color */}
+          <link
+            href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@300;400;500;700&display=swap"
+            rel="stylesheet"
+          ></link>
+          {/* Inject MUI styles first to match with the prepend: true configuration. */}
+          {this.props.emotionStyleTags}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
 
-export default MyDocument;
+// `getInitialProps` belongs to `_document` (instead of `_app`),
+// it's compatible with static-site generation (SSG).
+MyDocument.getInitialProps = async (ctx: DocumentContext) => {
+  const originalRenderPage = ctx.renderPage;
 
-// import createEmotionServer from "@emotion/server/create-instance";
-// import Document, { Head, Html, Main, NextScript } from "next/document";
-// import * as React from "react";
+  // You can consider sharing the same emotion cache between
+  // all the SSR requests to speed up performance.
+  // However, be aware that it can have global side effects.
 
-// import { getMetaThemeColor } from "../src/brandingTheme";
-// import createEmotionCache from "../src/createEmotionCache";
+  const cache = createEmotionCache();
+  const { extractCriticalToChunks } = createEmotionServer(cache);
 
-// export default class MyDocument extends Document {
-//   render() {
-//     return (
-//       <Html lang="en">
-//         <Head>
-//           <meta name="theme-color" content={getMetaThemeColor("light")} media="(prefers-color-scheme: light)" />
-//           <meta name="theme-color" content={getMetaThemeColor("dark")} media="(prefers-color-scheme: dark)" />
-//           <link rel="shortcut icon" href="/favicon.ico" />
-//           <link rel="preconnect" href="https://fonts.googleapis.com" />
-//           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-//           <meta name="google-site-verification" content="XpDB7dCQFlAHNdpUyVmw5Iahh4FzqPzUMUmlOLoNs-Y" />
-//           <meta charSet="UTF-8" />
-//           <meta name="robots" content="index, follow" />
-//           <meta name="googlebot" content="index, follow" />
-//           <meta name="language" content="ENG" />
-//           <meta name="copyright" content="1Cademy" />
-//           <meta name="reply-to" content="onecademy@umich.edu" />
-//           <meta name="coverage" content="Worldwide" />
-//           <meta name="distribution" content="Global" />
-//           <meta name="rating" content="General" />
-//           <meta name="target" content="all" />
-//           <meta name="HandheldFriendly" content="False" />
-//           <meta name="author" content="1Cademy" />
-//           <meta httpEquiv="Expires" content="-1" />
-//           <meta httpEquiv="Pragma" content="no-cache" />
-//           <meta httpEquiv="Cache-Control" content="no-cache" />
-//           <meta httpEquiv="x-dns-prefetch-control" content="off" />
-//           {/* PWA primary color */}
-//           <link
-//             href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@300;400;500;700&display=swap"
-//             rel="stylesheet"
-//           ></link>
-//           {/* Inject MUI styles first to match with the prepend: true configuration. */}
-//           {this.props.emotionStyleTags}
-//         </Head>
-//         <body>
-//           <Main />
-//           <NextScript />
-//         </body>
-//       </Html>
-//     );
-//   }
-// }
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: App =>
+        function EnhanceApp(props) {
+          return <App emotionCache={cache} {...props} />;
+        }
+    });
 
-// // `getInitialProps` belongs to `_document` (instead of `_app`),
-// // it's compatible with static-site generation (SSG).
-// MyDocument.getInitialProps = async ctx => {
-//   const originalRenderPage = ctx.renderPage;
+  const initialProps = await Document.getInitialProps(ctx);
 
-//   // You can consider sharing the same emotion cache between
-//   // all the SSR requests to speed up performance.
-//   // However, be aware that it can have global side effects.
+  // This is important. It prevents emotion to render invalid HTML.
+  // See
+  // https://github.com/mui-org/material-ui/issues/26561#issuecomment-855286153
 
-//   const cache = createEmotionCache();
-//   const { extractCriticalToChunks } = createEmotionServer(cache);
+  const emotionStyles = extractCriticalToChunks(initialProps.html);
+  const emotionStyleTags = emotionStyles.styles.map(style => (
+    <style
+      data-emotion={`${style.key} ${style.ids.join(" ")}`}
+      key={style.key}
+      // eslint-disable-next-line react/no-danger
+      dangerouslySetInnerHTML={{ __html: style.css }}
+    />
+  ));
 
-//   ctx.renderPage = () =>
-//     originalRenderPage({
-//       enhanceApp: App =>
-//         function EnhanceApp(props) {
-//           return <App emotionCache={cache} {...props} />;
-//         }
-//     });
-
-//   const initialProps = await Document.getInitialProps(ctx);
-
-//   // This is important. It prevents emotion to render invalid HTML.
-//   // See
-//   // https://github.com/mui-org/material-ui/issues/26561#issuecomment-855286153
-
-//   const emotionStyles = extractCriticalToChunks(initialProps.html);
-//   const emotionStyleTags = emotionStyles.styles.map(style => (
-//     <style
-//       data-emotion={`${style.key} ${style.ids.join(" ")}`}
-//       key={style.key}
-//       // eslint-disable-next-line react/no-danger
-//       dangerouslySetInnerHTML={{ __html: style.css }}
-//     />
-//   ));
-
-//   return {
-//     ...initialProps,
-//     emotionStyleTags
-//   };
-// };
+  return {
+    ...initialProps,
+    emotionStyleTags
+  };
+};
