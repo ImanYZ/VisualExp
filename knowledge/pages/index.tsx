04157f1269c51d96a6d7968f4ab62c4889b3e57f
Xavier Perez <xap5xap@users.noreply.github.com>
2022-06-01 15:50:27 -0500

    add tags search

--- knowledge/pages/index.tsx
+++ knowledge/pages/index.tsx
@@ -48,7 +48,8 @@ const buildSortBy = (upvotes: boolean, mostRecent: boolean) => {
   return `corrects:${!upvotes ? "asc" : "desc"}, updatedAt:${!mostRecent ? "asc" : "desc"}`;
 };
 
-const buildFilterBy = (timeWindow: TimeWindowOption) => {
+const buildFilterBy = (timeWindow: TimeWindowOption, tags: string) => {
+  const filters: string[] = [];
   let updatedAt: number = dayjs().subtract(1, "year").valueOf();
   if (timeWindow === TimeWindowOption.ThisWeek) {
     updatedAt = dayjs().subtract(1, "week").valueOf();
@@ -56,7 +57,12 @@ const buildFilterBy = (timeWindow: TimeWindowOption) => {
     updatedAt = dayjs().subtract(1, "month").valueOf();
   }
 
-  return `updatedAt:>${updatedAt}`;
+  filters.push(`updatedAt:>${updatedAt}`);
+  if (tags.length > 0) {
+    filters.push(`tags: [${tags}]`);
+  }
+
+  return filters.join("&& ");
 };
 
 export const getServerSideProps: GetServerSideProps<Props> = async ({ query }) => {
@@ -66,7 +72,7 @@ export const getServerSideProps: GetServerSideProps<Props> = async ({ query }) =
   const timeWindow: TimeWindowOption = existValueInEnum(TimeWindowOption, getQueryParameter(query.timeWindow))
     ? (getQueryParameter(query.timeWindow) as TimeWindowOption)
     : sortByDefaults.timeWindow;
-  // const tags = getQueryParameter(query.tags);
+  const tags = getQueryParameter(query.tags) || "";
   const page = getQueryParameterAsNumber(query.page);
   const client = new Typesense.Client({
     nodes: [
@@ -84,11 +90,12 @@ export const getServerSideProps: GetServerSideProps<Props> = async ({ query }) =
     per_page: perPage,
     page,
     sort_by: buildSortBy(upvotes, mostRecent),
-    filter_by: buildFilterBy(timeWindow)
+    filter_by: buildFilterBy(timeWindow, tags)
   };
   const searchResults = await client.collections<TypesenseNodesSchema>("nodes").documents().search(searchParameters);
   const nodeIds: string[] = searchResults.hits?.map(el => el.document.id) || [];
   const allPostsData = await getSortedPostsData(nodeIds);
+
   return {
     props: {
       data: allPostsData,
@@ -102,7 +109,6 @@ const HomePage: NextPage<Props> = ({ data, page, numResults }) => {
   const [sortedByUpvotes, setSortedByUpvotes] = useState(sortByDefaults.upvotes);
   const [sortedByMostRecent, setSortedByMostRecent] = useState(sortByDefaults.mostRecent);
   const [timeWindow, setTimeWindow] = useState(sortByDefaults.timeWindow);
-
   const router = useRouter();
 
   const handleSearch = (text: string) => {
@@ -128,10 +134,14 @@ const HomePage: NextPage<Props> = ({ data, page, numResults }) => {
     setTimeWindow(val);
   };
 
+  const handleTagsChange = (tags: string[]) => {
+    router.replace({ query: { ...router.query, tags: tags.join(",") } });
+  };
+
   return (
     <PagesNavbar>
       <HomeSearchContainer sx={{ mb: 1 }} onSearch={handleSearch} />
-      <HomeFilter></HomeFilter>
+      <HomeFilter onTagsChange={handleTagsChange}></HomeFilter>
       <Box sx={{ maxWidth: "1180px", margin: "auto", pt: "50px" }}>
         <SortByFilters
           upvotes={sortedByUpvotes}
