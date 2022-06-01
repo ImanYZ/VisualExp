7a2423efd1d56420a2b90952cb2693f8a1e485d5
Xavier Perez <xap5xap@users.noreply.github.com>
2022-06-01 17:36:55 -0500

    Add serach by institutions

--- knowledge/pages/index.tsx
+++ knowledge/pages/index.tsx
@@ -48,7 +48,7 @@ const buildSortBy = (upvotes: boolean, mostRecent: boolean) => {
   return `corrects:${!upvotes ? "asc" : "desc"}, updatedAt:${!mostRecent ? "asc" : "desc"}`;
 };
 
-const buildFilterBy = (timeWindow: TimeWindowOption, tags: string) => {
+const buildFilterBy = (timeWindow: TimeWindowOption, tags: string, institutions: string) => {
   const filters: string[] = [];
   let updatedAt: number = dayjs().subtract(1, "year").valueOf();
   if (timeWindow === TimeWindowOption.ThisWeek) {
@@ -60,6 +60,9 @@ const buildFilterBy = (timeWindow: TimeWindowOption, tags: string) => {
   filters.push(`updatedAt:>${updatedAt}`);
   if (tags.length > 0) {
     filters.push(`tags: [${tags}]`);
+  }
+  if (institutions.length > 0) {
+    filters.push(`institutions: [${institutions}]`);
   }
 
   return filters.join("&& ");
@@ -73,6 +76,7 @@ export const getServerSideProps: GetServerSideProps<Props> = async ({ query }) =
     ? (getQueryParameter(query.timeWindow) as TimeWindowOption)
     : sortByDefaults.timeWindow;
   const tags = getQueryParameter(query.tags) || "";
+  const institutions = getQueryParameter(query.institutions) || "";
   const page = getQueryParameterAsNumber(query.page);
   const client = new Typesense.Client({
     nodes: [
@@ -90,7 +94,7 @@ export const getServerSideProps: GetServerSideProps<Props> = async ({ query }) =
     per_page: perPage,
     page,
     sort_by: buildSortBy(upvotes, mostRecent),
-    filter_by: buildFilterBy(timeWindow, tags)
+    filter_by: buildFilterBy(timeWindow, tags, institutions)
   };
   const searchResults = await client.collections<TypesenseNodesSchema>("nodes").documents().search(searchParameters);
   const nodeIds: string[] = searchResults.hits?.map(el => el.document.id) || [];
@@ -138,10 +142,14 @@ const HomePage: NextPage<Props> = ({ data, page, numResults }) => {
     router.replace({ query: { ...router.query, tags: tags.join(",") } });
   };
 
+  const handleInstitutionsChange = (institutions: string[]) => {
+    router.replace({ query: { ...router.query, institutions: institutions.join(",") } });
+  };
+
   return (
     <PagesNavbar>
       <HomeSearchContainer sx={{ mb: 1 }} onSearch={handleSearch} />
-      <HomeFilter onTagsChange={handleTagsChange}></HomeFilter>
+      <HomeFilter onTagsChange={handleTagsChange} onInstitutionsChange={handleInstitutionsChange}></HomeFilter>
       <Box sx={{ maxWidth: "1180px", margin: "auto", pt: "50px" }}>
         <SortByFilters
           upvotes={sortedByUpvotes}
