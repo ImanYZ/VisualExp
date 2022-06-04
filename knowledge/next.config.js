797edc6cd752c132e85ab2380af4aefde3de5fd1
Xavier Perez <xap5xap@users.noreply.github.com>
2022-06-04 12:56:36 -0500

    /knowledge  is no longer the base path

--- knowledge/next.config.js
+++ knowledge/next.config.js
@@ -1,33 +1,5 @@
 module.exports = {
   reactStrictMode: true,
-  basePath: "/knowledge",
-  assetPrefix: "/knowledge/", // assetPrefix requires the trailing slash    ];
-  async headers() {
-    return [
-      {
-        // This works, and returns appropriate Response headers:
-        source: "/knowledge",
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "*"
-          },
-          {
-            key: "Pragma",
-            value: "no-cache"
-          },
-          {
-            key: "Expires",
-            value: "-1"
-          },
-          {
-            key: "Cache-Control",
-            value: "no-cache"
-          }
-        ]
-      }
-    ];
-  },
   images: {
     domains: ["firebasestorage.googleapis.com", "storage.googleapis.com"]
   }
