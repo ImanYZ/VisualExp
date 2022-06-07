9cc1cf6886d5677a9046263eac9105717cbfba05
jimy-nicanor <dev.jimynicanor@gmail.com>
2022-06-07 17:58:35 -0500

    finish adding share buttons to HomePage

--- knowledge/components/SortByFilters.tsx
+++ knowledge/components/SortByFilters.tsx
@@ -13,6 +13,7 @@ import { FC } from "react";
 
 import { SortedByTimeOptions } from "../lib/utils";
 import { TimeWindowOption } from "../src/knowledgeTypes";
+import { ShareButtons } from "./ShareButtons";
 
 type Props = {
   sortedByType: string;
@@ -37,32 +38,41 @@ const SortByFilters: FC<Props> = ({ sortedByType, handleByType, timeWindow, onTi
       justifyContent={{ xs: "center", sm: "space-between" }}
       gap="10px"
       sx={{
-        width: { xs: "100%", md: "750px" },
+        width: "100%",
         padding: { xs: "0px 40px", md: "0px 0px" },
         marginBottom: "50px"
       }}
     >
-      <ToggleButtonGroup value={sortedByType} exclusive onChange={handleSortByType} aria-label="Sort options" fullWidth>
-        <ToggleButton value="most-recent" aria-label="sort by the most recent">
-          Most Recent
-        </ToggleButton>
-        <ToggleButton value="upvotes-downvotes" aria-label="sort by upvotes">
-          Upvotes - Downvotes
-        </ToggleButton>
-      </ToggleButtonGroup>
+      <>
+        <ToggleButtonGroup
+          value={sortedByType}
+          exclusive
+          onChange={handleSortByType}
+          aria-label="Sort options"
+          fullWidth
+        >
+          <ToggleButton value="most-recent" aria-label="sort by the most recent">
+            Most Recent
+          </ToggleButton>
+          <ToggleButton value="upvotes-downvotes" aria-label="sort by upvotes">
+            Upvotes - Downvotes
+          </ToggleButton>
+        </ToggleButtonGroup>
 
-      <Tooltip title="Only show the nodes that were updated in this last period." placement="top">
-        <FormControl variant="filled" sx={{ m: 1, width: "100%" }} size="small">
-          <InputLabel id="any-time-label">Any Time</InputLabel>
-          <Select labelId="any-time-label" id="any-time" value={timeWindow} onChange={handleSortByTime}>
-            {SortedByTimeOptions.map((sortedByTimeOption, idx) => (
-              <MenuItem value={sortedByTimeOption} key={idx}>
-                {sortedByTimeOption}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-      </Tooltip>
+        <Tooltip title="Only show the nodes that were updated in this last period." placement="top">
+          <FormControl variant="filled" sx={{ m: 1, width: "100%" }} size="small">
+            <InputLabel id="any-time-label">Any Time</InputLabel>
+            <Select labelId="any-time-label" id="any-time" value={timeWindow} onChange={handleSortByTime}>
+              {SortedByTimeOptions.map((sortedByTimeOption, idx) => (
+                <MenuItem value={sortedByTimeOption} key={idx}>
+                  {sortedByTimeOption}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Tooltip>
+      </>
+      <ShareButtons />
     </Stack>
   );
 };
