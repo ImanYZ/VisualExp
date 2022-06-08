d2bb90dfac5a70389a02d8681498b382e62edc8a
Iman Yeckehzaare <oneweb@m-c02m21erf8jc.attlocal.net>
2022-06-08 16:29:32 -0400

    Fixed the Typesense search server issue and added stats to the homepage.

--- knowledge/scripts/typesenseConfig.ts
+++ knowledge/scripts/typesenseConfig.ts
@@ -5,7 +5,8 @@ dotenv.config();
 const config = {
   host: process.env.ONECADEMYCRED_TYPESENSE_HOST as string,
   port: parseInt(process.env.ONECADEMYCRED_TYPESENSE_PORT as string),
-  protocol: process.env.ONECADEMYCRED_TYPESENSE_PROTOCOL as string
+  protocol: process.env.ONECADEMYCRED_TYPESENSE_PROTOCOL as string,
+  apiKey: process.env.ONECADEMYCRED_TYPESENSE_APIKEY as string
 };
 
 export default config;
