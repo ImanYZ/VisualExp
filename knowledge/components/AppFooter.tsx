f0c3baaa71036f9084a8af4b5b1e223cf9111085
Xavier Perez <xap5xap@users.noreply.github.com>
2022-06-08 19:12:09 -0500

    Fix footer color

--- knowledge/components/AppFooter.tsx
+++ knowledge/components/AppFooter.tsx
@@ -1,4 +1,4 @@
-import { Stack } from "@mui/material";
+import { Divider, Stack } from "@mui/material";
 import Box from "@mui/material/Box";
 import Link from "@mui/material/Link";
 import Image from "next/image";
@@ -11,44 +11,51 @@ export default function AppFooter() {
     <Box
       component="footer"
       sx={{
-        height: "var(--footer-height)",
-        background: "#EDEDED",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center"
+        height: "var(--footer-height)"
       }}
     >
-      <Stack
-        direction={{ xs: "column", sm: "row" }}
-        spacing={{ xs: 1, sm: 2, md: 4 }}
-        sx={{ justifyContent: "center", alignItems: "center" }}
+      <Divider />
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "column",
+          alignItems: "center",
+          height: "100%"
+        }}
       >
-        <Box p={2}>{`©  1Cademy ${new Date().getFullYear()}`}</Box>
-        <Box p={2}>
-          <Link target="_blank" href="/terms" underline="none" sx={{ color: theme => theme.palette.common.black }}>
-            Terms
-          </Link>
-        </Box>
-        <Box p={2}>
-          <Link target="_blank" href="/privacy" underline="none" sx={{ color: theme => theme.palette.common.black }}>
-            Privacy
-          </Link>
-        </Box>
-        <Box p={2}>
-          <Link target="_blank" href="/cookie" underline="none" sx={{ color: theme => theme.palette.common.black }}>
-            Cookie
-          </Link>
-        </Box>
-        <Box
-          sx={{ display: "flex" }}
-          p={2}
-          component="a"
-          target="_blank"
-          href="https://www.youtube.com/channel/UCKBqMjvnUrxOhfbH1F1VIdQ/"
+        <Stack
+          direction={{ xs: "column", sm: "row" }}
+          spacing={{ xs: 1, sm: 2, md: 4 }}
+          sx={{ justifyContent: "center", alignItems: "center" }}
         >
-          <Image src={YouTubeLogo} alt="1Cademy YouTube Channel" layout="intrinsic" width="100px" height="40px" />
-        </Box>
-      </Stack>
+          <Box p={2}>{`©  1Cademy ${new Date().getFullYear()}`}</Box>
+          <Box p={2}>
+            <Link target="_blank" href="/terms" underline="none" sx={{ color: theme => theme.palette.common.black }}>
+              Terms
+            </Link>
+          </Box>
+          <Box p={2}>
+            <Link target="_blank" href="/privacy" underline="none" sx={{ color: theme => theme.palette.common.black }}>
+              Privacy
+            </Link>
+          </Box>
+          <Box p={2}>
+            <Link target="_blank" href="/cookie" underline="none" sx={{ color: theme => theme.palette.common.black }}>
+              Cookie
+            </Link>
+          </Box>
+          <Box
+            sx={{ display: "flex" }}
+            p={2}
+            component="a"
+            target="_blank"
+            href="https://www.youtube.com/channel/UCKBqMjvnUrxOhfbH1F1VIdQ/"
+          >
+            <Image src={YouTubeLogo} alt="1Cademy YouTube Channel" layout="intrinsic" width="100px" height="40px" />
+          </Box>
+        </Stack>
+      </Box>
     </Box>
   );
 }
