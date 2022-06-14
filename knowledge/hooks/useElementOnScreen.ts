1820497f11119da3e4b865aaeacbb7900e10b81f
jimy-nicanor <dev.jimynicanor@gmail.com>
2022-06-13 19:01:04 -0500

    configuring the intersection observer

--- knowledge/hooks/useElementOnScreen.ts
+++ knowledge/hooks/useElementOnScreen.ts
@@ -1,24 +1,40 @@
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 export const useElementOnScreen = (options: IntersectionObserverInit) => {
   const containerRef = useRef(null);
-  console.log("contREF", containerRef);
-  const [isVisible, setIsVisible] = useState(false);
+  const [isVisible] = useState(false);
 
-  const callbackFunction: IntersectionObserverCallback = entries => {
-    const [entry] = entries;
-    setIsVisible(entry.isIntersecting);
-  };
+  // const callbackFunction: IntersectionObserverCallback = (entries) => {
+  //   console.log('entries:', entries)
+  //   const [entry] = entries
+  //   setIsVisible(entry.isIntersecting)
+  // }
+
+  const rr = useCallback(
+    (res: any) => {
+      console.log("res", res);
+      return res;
+    },
+    [containerRef]
+  );
+
+  const r = rr(containerRef);
 
   useEffect(() => {
-    const observer = new IntersectionObserver(callbackFunction, options);
-    if (containerRef.current) observer.observe(containerRef.current);
+    console.log("useEffect...", containerRef, r);
+
+    const observer = new IntersectionObserver(([e]) => {
+      console.log("it works", e);
+    }, options);
+    if (r.current) {
+      console.log("exist current");
+      observer.observe(r.current);
+    }
 
     return () => {
-      console.log("REF", containerRef.current);
-      if (containerRef.current) observer.unobserve(containerRef.current);
+      if (r.current) observer.unobserve(r.current);
     };
-  }, [containerRef, options]);
+  });
 
-  return [containerRef, isVisible];
+  return { containerRef, isVisible };
 };
