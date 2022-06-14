5beb3ad6f274bcca2b0448a94fc56583b766d3e1
jimy-nicanor <dev.jimynicanor@gmail.com>
2022-06-14 18:30:42 -0500

    finish feedback desktop version

--- knowledge/lib/firestore/feedback.ts
+++
@@ -1,9 +0,0 @@
-import { addDoc, collection } from "firebase/firestore/lite";
-
-import { Feedback } from "../../src/knowledgeTypes";
-import { db } from "./firestoreClient.config";
-
-export const addFeedback = async (feedback: Feedback) => {
-  const docRef = await addDoc(collection(db, "feedback"), { feedback });
-  console.log("Document written with ID: ", docRef.id);
-};
