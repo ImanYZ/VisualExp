435493310bdf1ef24b6a374012af10db95ed0a7f
Xavier Perez <xap5xap@users.noreply.github.com>
2022-06-14 23:32:46 -0500

    add cypress configuration

--- knowledge/cypress.config.ts
+++ knowledge/cypress.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    viewportHeight: 1000,
+    viewportWidth: 1280,
+    defaultCommandTimeout: 20000,
+    requestTimeout: 20000,
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    }
+    setupNodeEvents(on, config) {}
   }
 });
