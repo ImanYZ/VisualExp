4511f232e010ef1815e931b7dfbfc38c0fa57cc2
Xavier Perez <xap5xap@users.noreply.github.com>
2022-06-14 23:01:19 -0500

    add script to open all nodes

--- knowledge/cypress/e2e/pregenerateNodes.cy.ts
+++ knowledge/cypress/e2e/pregenerateNodes.cy.ts
@@ -1,7 +1,22 @@
-describe("This file will go throu every node and it will visit it's page", () => {
-  it("Should visit all pages", () => {
-    // const numPages = 27536;
-    const numPages = 4;
+describe("This file will go through every node and it will visit it's page", () => {
+  it("should visit every node's page", () => {
+    cy.readFile("./pages.txt").then(str => {
+      // parse the string into object literal
+      const res = str.split(",");
+      for (let nodepage of res) {
+        console.log(`visting page: ${nodepage}`);
+        cy.log(`visting page: ${nodepage}`);
+        cy.visit(`http://localhost:3000${nodepage}`);
+        // cy.visit(`https://node.1cademy.us${nodepage}`);
+        cy.findByTestId("node-item-full").should("exist");
+        // cy.wait(2000);
+      }
+    });
+  });
+
+  it.skip("Should visit all pages", () => {
+    const numPages = 27536;
+    // const numPages = 4;
     cy.visit(`http://localhost:3000/`);
 
     for (let i = 0; i < numPages; i++) {
