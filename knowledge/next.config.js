4c88f0bc7b1e4db8aeb3be955b2940715788dd26
Xavier Perez <xap5xap@users.noreply.github.com>
2022-06-14 19:47:55 -0500

    configure cypress

--- knowledge/next.config.js
+++ knowledge/next.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  ignorePatterns: ["cypress/**"],
   reactStrictMode: true,
   images: {
     domains: ["firebasestorage.googleapis.com", "storage.googleapis.com", "node.1cademy.us", "1cademy.us"]
