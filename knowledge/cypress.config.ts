7458a6fe66d4d34865ef0e0fb356bc923a3bbc23
Xavier Perez <xap5xap@users.noreply.github.com>
2022-06-18 11:03:20 -0500

    add homepage tests

--- knowledge/cypress.config.ts
+++ knowledge/cypress.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    baseUrl: "http://localhost:3000",
+    specPattern: "cypress/tests/**/*.spec.{js,jsx,ts,tsx}",
+    supportFile: "cypress/support/e2e.ts",
     viewportHeight: 1000,
     viewportWidth: 1280,
     defaultCommandTimeout: 20000,
