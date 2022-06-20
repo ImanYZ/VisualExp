e6790df4124bcac98fa8755bf5d35c1aff689319
Xavier Perez <xap5xap@users.noreply.github.com>
2022-06-20 15:48:03 -0500

    add jest coverage

--- knowledge/jest.config.js
+++ knowledge/jest.config.js
@@ -9,7 +9,17 @@ const customJestConfig = {
   setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect", "@testing-library/react", "<rootDir>/jest.setup.ts"],
   moduleDirectories: ["node_modules", "<rootDir>/"],
   testEnvironment: "jest-environment-jsdom",
-  testMatch: ["**/__tests__/**/*.[jt]s?(x)"]
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)"],
+  collectCoverage: true,
+  collectCoverageFrom: ["components/**/*.{js,jsx,ts,tsx}", "pages/**/*.{js,jsx,ts,tsx}"],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  }
 };
 
 module.exports = createJestConfig(customJestConfig);
