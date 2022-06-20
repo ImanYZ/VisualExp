784bb9b8f2c10841087978bd9217ce8fb40ecf8a
jimy-nicanor <dev.jimynicanor@gmail.com>
2022-06-20 16:24:51 -0500

    finish sugestions searcher sorting by sugestion with start keyword

--- knowledge/components/SearchInput.tsx
+++ knowledge/components/SearchInput.tsx
@@ -3,6 +3,10 @@ import { Autocomplete, IconButton } from "@mui/material";
 import { Box } from "@mui/system";
 import { useRouter } from "next/router";
 import { FC, useEffect, useState } from "react";
+import { useQuery } from "react-query";
+import { useDebounce } from "use-debounce";
+
+import { getSearchAutocomplete } from "../lib/knowledgeApi";
 
 type Props = {
   onSearch: (text: string) => void;
@@ -11,13 +15,25 @@ type Props = {
 
 const SearchInput: FC<Props> = ({ onSearch }) => {
   const router = useRouter();
-  const [searchText, setSearchText] = useState<string>((router.query.q as string) || "");
+  const [text, setText] = useState<string>((router.query.q as string) || "");
+  const [searchText] = useDebounce(text, 250);
 
-  useEffect(() => setSearchText((router.query.q as string) || ""), [router.query]);
+  const { data, isLoading } = useQuery(["searchAutocomplete", searchText], () => getSearchAutocomplete(searchText));
+
+  useEffect(() => {
+    setText((router.query.q as string) || "");
+  }, [router.query]);
 
   const handleSearch = (e: React.FormEvent) => {
+    (document.activeElement as HTMLElement).blur();
     e.preventDefault();
-    onSearch(searchText);
+    onSearch(text);
+  };
+
+  const handleChangeOption = (value: string) => {
+    (document.activeElement as HTMLElement).blur();
+    setText(value);
+    onSearch(value);
   };
 
   return (
@@ -25,8 +41,11 @@ const SearchInput: FC<Props> = ({ onSearch }) => {
       <Autocomplete
         id="custom-input-demo"
         fullWidth
-        options={[]}
+        options={data?.results || []}
         freeSolo={true}
+        loading={isLoading}
+        onChange={(e, value) => handleChangeOption(value || "")}
+        openOnFocus={true}
         sx={{
           display: "inline-block",
           fontSize: "inherit",
@@ -36,6 +55,7 @@ const SearchInput: FC<Props> = ({ onSearch }) => {
             fontSize: { xs: "16px", md: "25px" },
             fontWeight: 300,
             border: "none",
+            color: theme => theme.palette.common.black,
             background: theme => theme.palette.common.white
           },
           "& input:focus": {
@@ -54,14 +74,16 @@ const SearchInput: FC<Props> = ({ onSearch }) => {
             ref={params.InputProps.ref}
           >
             <input
-              value={searchText}
-              onChange={e => setSearchText(e.target.value)}
-              placeholder="Search on 1Cademy "
               {...params.inputProps}
+              value={text}
+              onChange={e => {
+                setText(e.target.value);
+              }}
+              placeholder="Search on 1Cademy "
             />
             <IconButton
               type="submit"
-              sx={{ p: "5px", color: theme => theme.palette.common.black, fontSize: "inherit" }}
+              sx={{ p: "5px", color: "#757575", fontSize: "inherit" }}
               aria-label="search"
               onClick={handleSearch}
             >
