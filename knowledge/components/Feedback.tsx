8d17658f1dcd783b6c4e6e5674ff2921b86386a7
jimy-nicanor <dev.jimynicanor@gmail.com>
2022-06-22 12:07:35 -0500

    move feedback button to bottom of page

--- knowledge/components/Feedback.tsx
+++ knowledge/components/Feedback.tsx
@@ -147,7 +147,7 @@ export const Feedback = forwardRef<Ref, FeedbackProps>(({ onSuccessFeedback, sx 
                   value={values.email}
                   onChange={handleChange}
                   onBlur={handleBlur}
-                  isError={Boolean(errors.email) && Boolean(touched.email)}
+                  error={Boolean(errors.email) && Boolean(touched.email)}
                 />
               </FormControl>
 
@@ -161,7 +161,7 @@ export const Feedback = forwardRef<Ref, FeedbackProps>(({ onSuccessFeedback, sx 
                   value={values.name}
                   onChange={handleChange}
                   onBlur={handleBlur}
-                  isError={Boolean(errors.name) && Boolean(touched.name)}
+                  error={Boolean(errors.name) && Boolean(touched.name)}
                 />
               </FormControl>
 
@@ -177,7 +177,7 @@ export const Feedback = forwardRef<Ref, FeedbackProps>(({ onSuccessFeedback, sx 
                   onBlur={handleBlur}
                   rows={3}
                   multiline
-                  isError={Boolean(errors.feedback) && Boolean(touched.feedback)}
+                  error={Boolean(errors.feedback) && Boolean(touched.feedback)}
                 />
               </FormControl>
               <LoadingButton type="submit" color="primary" variant="contained" fullWidth loading={isSubmitting}>
@@ -210,7 +210,7 @@ export const Feedback = forwardRef<Ref, FeedbackProps>(({ onSuccessFeedback, sx 
 
 Feedback.displayName = "Feedback";
 
-const CustomInput = styled(InputBase)<{ isError: boolean }>(({ theme, isError }) => ({
+const CustomInput = styled(InputBase)<{ error: boolean }>(({ theme, error }) => ({
   "label + &": {
     marginTop: theme.spacing(3),
     color: theme.palette.common.white
@@ -218,9 +218,9 @@ const CustomInput = styled(InputBase)<{ isError: boolean }>(({ theme, isError })
   "& .MuiInputBase-input": {
     borderRadius: 4,
     position: "relative",
-    backgroundColor: isError ? "#FF000080" : "#515153",
+    backgroundColor: error ? "#FF000080" : "#515153",
     border: "solid 1px",
-    borderColor: isError ? "#FF0000" : "#515153",
+    borderColor: error ? "#FF0000" : "#515153",
     fontSize: 16,
     width: "100%",
     padding: "10px 12px",
