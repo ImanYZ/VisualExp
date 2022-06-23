f3f6e5c99c73f7c066153d1312fd0158d9a9ae48
Usama Sehezad <usamashzd99@gmail.com>
2022-06-24 00:38:11 +0500

    free recall grading page passage loading on project spec fixed.

--- expHome/src/Components/firebase/config.js
+++ expHome/src/Components/firebase/config.js
@@ -1,5 +1,16 @@
-// Your web app's Firebase configuration
-export const firebaseConfig = {
+
+const firebaseDevConfig = {
+  apiKey: "AIzaSyCLYan1Q1vda3eed70rRoajMLUSHZfySFc",
+  authDomain: "visualexp-5d2c6.firebaseapp.com",
+  projectId: "visualexp-5d2c6",
+  storageBucket: "visualexp-5d2c6.appspot.com",
+  messagingSenderId: "1088807277176",
+  appId: "1:1088807277176:web:48a3003ddd629bcb07138d",
+  measurementId: "G-075YRXFSRR"
+}
+
+
+const firebaseProdConfig = {
   apiKey: "AIzaSyBp9kzfQ-g1qdld9W68Bmrs_aS7rgSWFHs",
   authDomain: "visualexp-a7d2c.firebaseapp.com",
   projectId: "visualexp-a7d2c",
@@ -8,6 +19,9 @@ export const firebaseConfig = {
   appId: "1:141114383555:web:0668953725936df8f11676",
   measurementId: "G-0BH5YRED86",
 };
+
+// Your web app's Firebase configuration
+export const firebaseConfig = process.env.NODE_ENV === 'development' ? firebaseDevConfig : firebaseProdConfig;
 
 export const firebaseOneConfig = {
   apiKey: "AIzaSyAl1Lfmndsmvax6PZVH48nwV0kEaBOVgDE",
