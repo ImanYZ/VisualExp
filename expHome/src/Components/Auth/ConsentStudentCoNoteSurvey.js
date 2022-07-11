dd5c353ae5091ea43cd19dd40a26f4b3fe5e0870
Iman <oneweb@umich.edu>
2022-07-11 08:20:53 -0400

    Added the consent from for the project StudentCoNoteSurvey and separated a route for this new project sign In/Up.

---
+++ expHome/src/Components/Auth/ConsentStudentCoNoteSurvey.js
@@ -0,0 +1,110 @@
+import React from "react";
+
+import "./ConsentDocument.css";
+
+const ConsentDocument = props => {
+  return (
+    <div id="ConsentContainer">
+      <h1>University of Michigan</h1>
+      <h1>Consent To Be Part Of A Research Study</h1>
+      <h2>Title of Project: A Survey Study on Study Techniques</h2>
+      <h2>eResearch ID: HUM00166649</h2>
+
+      <h3>Principal Investigator</h3>
+      <p>
+        <strong>Iman YeckehZaare</strong>, Ph.D. Candidate, University of Michigan, School of Information
+      </p>
+
+      <h3>Faculty Advisors</h3>
+      <p>
+        <strong>Paul Resnick</strong>, Michael D. Cohen Collegiate Professor of Information and Associate Dean for
+        Research and Faculty Affairs, University of Michigan School of Information
+      </p>
+      <p>
+        <strong>Christopher Brooks</strong>, Assistant Professor, University of Michigan School of Information
+      </p>
+
+      <h3>Research Team</h3>
+      <ul>
+        <li>
+          <strong>Benjamin Brown</strong>, Graduate student at San Jose State University, School of Information
+        </li>
+        <li>
+          <strong>Lily DiBartolomeo</strong>, Undergraduate, New York University, College of Arts and Sciences,
+          Psychology
+        </li>
+        <li>
+          <strong>Alexandra Bateman</strong>, Undergraduate, Creighton University Honors Program, Psychology
+        </li>
+      </ul>
+
+      <h2>GENERAL Information</h2>
+      <h3>2. PURPOSE OF THIS STUDY</h3>
+      <p>
+        <strong>2.1 Study purpose:</strong>
+      </p>
+      <p>
+        We are conducting a study to learn more about the helpful/unhelpful study strategies that students use in order
+        to inform school administrators, instructors, and students about the helpful study techniques. To get
+        information, we would appreciate your input on your personal experience with using study strategies in Winter
+        2022. We are also studying factors such as gender and achievement that might influence which strategies are
+        used.
+      </p>
+      <p>
+        <strong>What will I do?</strong>
+      </p>
+      <p>
+        You will fill out a survey that will ask you various questions about the study strategies you used throughout
+        the past semester and your thoughts on those and other strategies. We expect the survey to take on average 45
+        minutes to complete. You do not have to answer it if you’d rather not. You can skip any questions that you do
+        not want to answer and you do not need to explain the reason.
+      </p>
+      <p>
+        If you agree to be part of the research study, you will be asked to attend a recorded one-on-one online
+        interview session where the interviewer asks the interviewee to share their screen to show them how they answer
+        different types of questions on our experiment webpage and explain their answers to each question.
+      </p>
+      <p>
+        <strong>Is Participation Required?</strong>
+      </p>
+      <p>
+        No, this study is completely voluntary. However, only those who complete the survey will be entered into the
+        raffle.
+      </p>
+      <p>
+        <strong>Compensation</strong>
+      </p>
+      <p>
+        The research team will select ten participants for their thoughtful responses on the survey and will send each a
+        $100 Amazon Gift Card.
+      </p>
+      <p>
+        There are no risks, however, you will be asked to share your screen to present how you read the passages and
+        answer questions, and talk throughout the recorded interview.
+      </p>
+      <p>There is no compensation for participation in this research.</p>
+      <p>
+        Participating in this study is completely voluntary. Even if you decide to participate now, you may change your
+        mind and stop at any time. You may choose not to answer any or request to pause or stop the interview for any
+        reason.
+      </p>
+      <p>We will not collect any sensitive or identifiable information.</p>
+      <p>
+        Information collected in this project may be shared with other researchers, but we will not share any
+        information that could identify you.
+      </p>
+      <p>If you have questions about this research study, please contact:</p>
+      <p>Principal Investigator: Iman YeckehZaare</p>
+      <p>Email: oneweb@umich.edu</p>
+      <p>Phone: (+1) (734) 647-6333</p>
+      <p>Faculty Advisor: Paul Resnick</p>
+      <p>Email: presnick@umich.edu</p>
+      <p>
+        As part of their review, the University of Michigan Institutional Review Board Health Sciences and Behavioral
+        Sciences has determined that this study is no more than minimal risk and exempt from ongoing IRB oversight.
+      </p>
+    </div>
+  );
+};
+
+export default ConsentDocument;
