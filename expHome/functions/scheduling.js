eb725b8972ea6ec7e9cceca92b94e41b1fa1b8cb
Usama Sehezad <usamashzd99@gmail.com>
2022-07-13 04:16:57 +0500

    manage even page updated

--- expHome/functions/scheduling.js
+++ expHome/functions/scheduling.js
@@ -7,7 +7,7 @@ const { insertEvent, getEvent, getEvents, deleteEvent, insertLifeLogEvent } = re
 const { pad2Num } = require("./utils");
 const { toOrdinal } = require("number-to-words");
 
-const createExperimentEvent = async (email, researcher, order, start, end) => {
+const createExperimentEvent = async (email, researcher, order, start, end, projectSpecs) => {
   const summary = "[1Cademy] UX Research Experiment - " + order + " Session";
   const description =
     "<div><p><strong><u>IMPORTANT: On your Internet browser, please log in to Gmail using your " +
@@ -15,9 +15,11 @@ const createExperimentEvent = async (email, researcher, order, start, end) => {
     " credentials before entering this session. If you have logged in using your other email addresses, Google Meet will not let you in!</u></strong></p><P>This is your " +
     order +
     " session of the UX Research experiment.</p>" +
-    (order === "1st"
+    // projectSpecs.numberOfSessions === 3 just a temporary quick thing,
+    // a proper solution will be implemented for annotating project
+    (order === "1st" && projectSpecs.numberOfSessions === 3
       ? "<p>We've also scheduled your 2nd session 3 days later, and 3rd session one week later.</p>"
-      : order === "2nd"
+      : order === "2nd" && projectSpecs.numberOfSessions === 3
       ? "<p>We've also scheduled your 3rd session 4 days later.</p>"
       : "") +
     "<p><strong><u>Please confirm your attendance in this session by accepting the invitation on Google Calendar or through the link at the bottom of the invitation email.</u></strong></p>" +
@@ -47,7 +49,14 @@ exports.schedule = async (req, res) => {
         const start = new Date(sess.startDate);
         // adding slotDuration * number of slots for the session
         const end = new Date(start.getTime() + slotDuration * (projectSpecsData.sessionDuration?.[i] || 2) * 60000);
-        const eventCreated = await createExperimentEvent(email, sess.researcher, toOrdinal(i + 1), start, end);
+        const eventCreated = await createExperimentEvent(
+          email,
+          sess.researcher,
+          toOrdinal(i + 1),
+          start,
+          end,
+          projectSpecsData
+        );
         events.push(eventCreated);
       }
       return res.status(200).json({ events });
@@ -68,19 +77,27 @@ exports.scheduleSingleSession = async (req, res) => {
       req.body.researcher &&
       "order" in req.body &&
       req.body.order &&
-      "session" in req.body
+      "session" in req.body &&
+      "project" in req.body &&
+      "sessionIndex" in req.body
     ) {
       const email = req.body.email;
       const researcher = req.body.researcher;
       const order = req.body.order;
       const start = new Date(req.body.session);
-      let end;
-      if (order === "1st") {
-        end = new Date(start.getTime() + 60 * 60000);
-      } else if (order === "2nd" || order === "3rd") {
-        end = new Date(start.getTime() + 30 * 60000);
+
+      const projectSpecs = await db.collection("projectSpecs").doc(req.body.project).get();
+      if (!projectSpecs.exists) {
+        throw new Error("Project Specs not found.");
       }
-      const eventCreated = await createExperimentEvent(email, researcher, order, start, end);
+      const projectSpecsData = projectSpecs.data();
+      // 1 hour / 2 = 30 mins
+      const slotDuration = 60 / (projectSpecsData.hourlyChunks || 2);
+
+      const end = new Date(
+        start.getTime() + slotDuration * (projectSpecsData.sessionDuration?.[req.body.sessionIndex] || 1) * 60000
+      );
+      const eventCreated = await createExperimentEvent(email, researcher, order, start, end, projectSpecsData);
       return res.status(200).json({ events: [eventCreated] });
     }
   } catch (err) {
