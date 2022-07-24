f75adb3cfb12c8455e8641b69452eefd64823ebb
sam ouhra <ouhrac@gmail.com>
2022-07-24 14:47:42 +0100

    integrate the qualitative coding in the post questions for the new expriment

--- expHome/src/Components/Passage/PostQuestionsPage.js
+++ expHome/src/Components/Passage/PostQuestionsPage.js
@@ -2,24 +2,12 @@ import React, { useState, useEffect } from "react";
 import { firebaseState, fullnameState } from "../../../src/store/AuthAtoms";
 import { useRecoilValue, useRecoilState } from "recoil";
 import MCQuestion from "../MCQuestion/MCQuestion";
-import QualitativeCoding from "./qualitativeCoding/QualitativeCoding";
 import "./Passage.css";
 
-const PostQuestionsPage = props => {
-  const fullname = useRecoilValue(fullnameState);
-  const firebase = useRecoilValue(firebaseState);
-  const [experimentType, setExperimentType] = useState();
 
-  useEffect(async () => {
-    const userDoc = await firebase.db.collection("users").doc(fullname).get();
-    const experiment = ["easyResponse", "activeCoding"];
-    const random = Math.floor(Math.random() * experiment.length);
-    setExperimentType("activeCoding");
-    console.log(random, experiment[random]);
-  }, []);
-
-  if (experimentType === "easyResponse") {
+const PostQuestionsPage = (props) => {
     return (
+      <>
       <div id="QuestionsInstructions">
         <MCQuestion
           setCurrentQIdx={props.setCurrentQIdx}
@@ -28,20 +16,15 @@ const PostQuestionsPage = props => {
           nextStep={props.nextStep}
           explanations={props.explanations}
           setExplanations={props.setExplanations}
+          step={props.step}
         />
       </div>
+   
+    </>
+       
     );
-  }
-  if (experimentType === "activeCoding") {
-    return (
-      <div id="QuestionsInstructions">
-        <QualitativeCoding
-          currentQIdx={props.currentQIdx}
-          questions={props.questions} />
-      </div>
-    );
-  }
-  return null;
+
+
 };
 
 export default PostQuestionsPage;
