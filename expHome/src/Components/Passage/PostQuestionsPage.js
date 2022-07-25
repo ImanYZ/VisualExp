103ef26a2db812ccdd1dced55586aa79ecedae80
Yash Gandhi <yashgandhi303@gmail.com>
2022-07-26 03:21:27 +0530

    review changes eslint

--- expHome/src/Components/Passage/PostQuestionsPage.js
+++ expHome/src/Components/Passage/PostQuestionsPage.js
@@ -1,30 +1,22 @@
-import React, { useState, useEffect } from "react";
-import { firebaseState, fullnameState } from "../../../src/store/AuthAtoms";
-import { useRecoilValue, useRecoilState } from "recoil";
+import React from "react";
 import MCQuestion from "../MCQuestion/MCQuestion";
 import "./Passage.css";
 
 
 const PostQuestionsPage = (props) => {
-    return (
-      <>
-      <div id="QuestionsInstructions">
-        <MCQuestion
-          setCurrentQIdx={props.setCurrentQIdx}
-          currentQIdx={props.currentQIdx}
-          questions={props.questions}
-          nextStep={props.nextStep}
-          explanations={props.explanations}
-          setExplanations={props.setExplanations}
-          step={props.step}
-        />
-      </div>
-   
-    </>
-       
-    );
-
-
+  return (
+    <div id="QuestionsInstructions">
+      <MCQuestion
+        setCurrentQIdx={props.setCurrentQIdx}
+        currentQIdx={props.currentQIdx}
+        questions={props.questions}
+        nextStep={props.nextStep}
+        explanations={props.explanations}
+        setExplanations={props.setExplanations}
+        step={props.step}
+      />
+    </div>
+  );
 };
 
 export default PostQuestionsPage;
