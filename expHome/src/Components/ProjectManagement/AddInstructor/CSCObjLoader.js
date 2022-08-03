cf1975ab9b3c80f04e0171bb4e2f472319fbcf82
Sam ouhra <ouhrac@gmail.com>
2022-08-03 22:14:19 +0100

    fix the issue  with instructors country state city  drop up menu

--- expHome/src/Components/ProjectManagement/AddInstructor/CSCObjLoader.js
+++ expHome/src/Components/ProjectManagement/AddInstructor/CSCObjLoader.js
@@ -1,19 +1,15 @@
-import { useEffect, useCallback } from "react";
 
-const CSCObjLoader = (CSCObj, setCSCObj, setAllCountries) => {
-  const loadCSCObj = useCallback(async () => {
+
+const CSCObjLoader = async  (CSCObj, setCSCObj, setAllCountries) => {
+
     if (!CSCObj) {
       const cscObj = await import("country-state-city");
       setCSCObj(cscObj);
       setAllCountries(cscObj.Country.getAllCountries());
     }
-  }, [CSCObj, setCSCObj, setAllCountries]);
 
-  useEffect(() => {
-    loadCSCObj();
-  }, []);
 
-  return loadCSCObj;
+
 };
 
 export default CSCObjLoader;
