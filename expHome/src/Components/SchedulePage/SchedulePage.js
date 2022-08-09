486af1decec1d4e3562db706723bb7b60650d1d2
Iman <oneweb@umich.edu>
2022-08-08 20:39:04 -0400

    fixed the typo in schedulePgage.js

--- expHome/src/Components/SchedulePage/SchedulePage.js
+++ expHome/src/Components/SchedulePage/SchedulePage.js
@@ -352,7 +352,7 @@ const SchedulePage = props => {
             {i + 1}
             <sup>{toOrdinal(i + 1).replace(/[0-9]/g, "")}</sup> session
           </strong>{" "}
-          {i > 0 ? `, ${projectSpecs.daysLater[i - 1]} days latter after the first session,` : null}
+          {i > 0 ? `, ${projectSpecs.daysLater[i - 1]} days later after the first session,` : null}
           {" " + formatSlotTime(projectSpecs.hourlyChunks, projectSpecs?.sessionDuration?.[i], i)}
         </li>
       );
