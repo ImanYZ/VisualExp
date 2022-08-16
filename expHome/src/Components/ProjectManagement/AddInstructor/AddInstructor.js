fbbb0aafbcbd6cd924f9b7ff90c73bd8a319c8bc
Sam ouhra <ouhrac@gmail.com>
2022-08-16 13:57:16 +0100

    fix upvoting instructors does not work

--- expHome/src/Components/ProjectManagement/AddInstructor/AddInstructor.js
+++ expHome/src/Components/ProjectManagement/AddInstructor/AddInstructor.js
@@ -893,7 +893,6 @@ const AddInstructor = props => {
             vote: clickedCell.field,
             voterProject: project
           });
-          setComment("");
           setSnackbarMessage("You successfully voted for others' instructor/administrator!");
         }
       } catch (err) {
@@ -914,8 +913,10 @@ const AddInstructor = props => {
         await axios.post("/voteInstructor", {
           instructor: instructorId,
           vote: voteType,
+          voterProject: project,
           comment
         });
+        setComment("");
         setSnackbarMessage("You successfully voted for others' instructor/administrator!");
         setOtherVoting(false);
       }
