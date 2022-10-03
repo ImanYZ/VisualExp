3c56529da5f2f2c66af037120e07fcbab882f334
Sam ouhra <ouhrac@gmail.com>
2022-10-03 16:37:33 +0100

    change the UI for the booleane opertaors and the data structure of it

--- expHome/src/Components/ProjectManagement/SchemaGeneration/SchemaGeneration.js
+++ expHome/src/Components/ProjectManagement/SchemaGeneration/SchemaGeneration.js
@@ -7,41 +7,48 @@ import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Select from "@mui/material/Select";
-import { styled } from "@mui/material/styles";
+import { alpha, styled } from "@mui/material/styles";
 import Typography from "@mui/material/Typography";
 import MenuItem from "@mui/material/MenuItem";
 import Button from "@mui/material/Button";
 import SaveIcon from "@mui/icons-material/Save";
 import DeleteIcon from "@mui/icons-material/Delete";
-import ArrowRightIcon from '@mui/icons-material/ArrowRight';
-import ArrowLeftIcon from '@mui/icons-material/ArrowLeft';
+import ArrowRightIcon from "@mui/icons-material/ArrowRight";
+import ArrowLeftIcon from "@mui/icons-material/ArrowLeft";
 import IconButton from "@mui/material/IconButton";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
-
+import TreeView from "@mui/lab/TreeView";
+import TreeItem, { treeItemClasses } from "@mui/lab/TreeItem";
+import PropTypes from "prop-types";
+// import { useSpring, animated } from "react-spring/web.cjs";
+import Collapse from "@mui/material/Collapse";
+import SvgIcon from "@mui/material/SvgIcon";
+import AddIcon from "@mui/icons-material/Add";
 import "./SchemaGeneration.css";
+import Checkbox from "@mui/material/Checkbox";
 
-const initialSchema = {
-  ands: ["and1", "and2"],
-  ors: ["or1", "or2"],
-  nots: ["not1", "not2"]
+const temp_schema = {
+  id:"1",
+  combinator: "AND",
+  negations: [true, false],
+  keyWords: [
+    {
+      id:"2",
+      combinator: "OR",
+      negations: [true, false],
+      keyWords: ["skull", "crani"]
+    },
+    {
+      id:"3",
+      combinator: "OR",
+      negations: [true, false],
+      keyWords: ["skull", "crani"]
+    },
+    "prey",
+    "mouse"
+  ],
 };
-
-const temp_schema = [
-  {
-    combinator: "OR",
-    keyWords: ["prey", "mouse"]
-  },
-  {
-    combinator: "AND",
-    keyWords: ["and", "and"]
-  },
-  {
-    combinator: "NOT",
-    operator: "OR",
-    keyWords: ["skull", "crani"]
-  }
-];
 
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
@@ -51,36 +58,38 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 // eslint-disable-next-line no-empty-pattern
-export const SchemaGeneration = ({ }) => {
+export const SchemaGeneration = ({}) => {
   const firebase = useRecoilValue(firebaseState);
   const fullname = useRecoilValue(fullnameState);
   const [passages, setPassages] = useState([]);
   const [phrases, setSelectedPhrases] = useState([]);
   const [selectedPassage, setSelectedPassage] = useState({});
   const [selectedPhrase, setSelectedPhrase] = useState(null);
-  const [schema, setSchema] = useState(initialSchema);
-
+  const [schema, setSchema] = useState(temp_schema);
+  const [select, setSelect] = useState("AND");
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(async () => {
     const passagesDocs = await firebase.db.collection("passages").get();
     // const passageData = passagesDoc.data();
     let passages = [];
-    passages = passagesDocs.docs.map(x => {
-      const data = x.data();
-      if (data?.phrases?.length > 0) {
-        return { ...data, id: x.id };
-      }
-      return null;
-    }).filter(x => x !== null)
-    console.log({ passages })
+    passages = passagesDocs.docs
+      .map(x => {
+        const data = x.data();
+        if (data?.phrases?.length > 0) {
+          return { ...data, id: x.id };
+        }
+        return null;
+      })
+      .filter(x => x !== null);
+    // console.log({ passages });
     setPassages(passages);
     setSelectedPassage(passages[0]);
     const phrases = passages[0].phrases;
     setSelectedPhrases([...phrases]);
     setSelectedPhrase(phrases[0]);
-  }, [firebase.db, fullname])
+  }, [firebase.db, fullname]);
 
-  const handlePassageChange = (event) => {
+  const handlePassageChange = event => {
     const title = event.target.value;
     const allPassages = [...passages];
     const fIndex = allPassages.findIndex(i => i.title === title);
@@ -88,58 +97,70 @@ export const SchemaGeneration = ({ }) => {
     setSelectedPassage(passage);
     setSelectedPhrases(passage.phrases);
     setSelectedPhrase(passage.phrases[0]);
-  }
-
-  const handlePhraseChange = (event) => {
-    const phrase = event.target.value;
-    const allPhrases = [...phrases];
-    const fIndex = allPhrases.findIndex(i => i === phrase);
-    const selectedPhrase = allPhrases[fIndex];
-    setSelectedPhrase(selectedPhrase);
-  }
-
-  const onAddAnd = () => {
-    const allSchema = { ...schema };
-    allSchema.ands.push("and");
-    setSchema(allSchema)
-  }
-
-  const onAddOR = () => {
-    const allSchema = { ...schema };
-    allSchema.ors.push("or");
-    setSchema(allSchema)
-  }
-
-  const onAddNot = () => {
-    const allSchema = { ...schema };
-    allSchema.nots.push("not");
-    setSchema(allSchema)
-  }
-
-  const renderOperators = ({ op, not }) => {
-    if (op && op?.keyWords && op?.keyWords?.length > 0) {
-      return (
-        <>
-          {not && <span>not</span>}
-          {op.keyWords.map((x, index) => {
-            return (
-              <div key={index}>
-                <input value={x} />
-                {op.keyWords.length !== (index + 1) && <span>{op?.combinator?.toLowerCase()}</span>}
-              </div>
-            );
-          })
-          }
-        </>
-      );
-    }
-    return null;
   };
 
-  if (passages.length <= 0) return null;
+  const handleChange = event => {
+    setSelect(event.target.value);
+  };
 
-  console.log({ selectedPhrase });
+  const addRule = () => {
+    const tmpSchma = { ...schema };
+    tmpSchma.keyWords.push({
+      combinator: "OR",
+      negations: [true, false],
+      keyWords: ["skull", "crani"]
+    });
+    setSchema(tmpSchma);
+  };
 
+  const addKeyword = () => {
+    const tmpSchma = { ...schema };
+    tmpSchma.keyWords.push("key");
+    setSchema(tmpSchma);
+    console.log(tmpSchma);
+  };
+
+  const renderSchema = (tempSchema) => {
+    return (
+      <Paper elevation={3} sx={{ minheight: "300px", width: "97%", padding: "10px" }}>
+        <div style={{ flexDirection: "row", marginLeft: "500px", alignItems: "flex-start" }}>
+          <Button onClick={addKeyword}>ADD A KEYWORD</Button>
+          <Button onClick={addRule}>ADD A RULE</Button>
+        </div>
+        <div class="clt">
+          <ul>
+            <li>
+              <Box sx={{ marginRight: "890px" }}>
+                <div style={{ padding: "0px" }}>
+                  <select value={select} onChange={handleChange}>
+                    <option value="AND">AND</option>
+                    <option value="OR">OR</option>
+                  </select>
+                </div>
+              </Box>
+            </li>
+            {tempSchema?.keyWords?.map(element => {
+              return (
+                <li>
+                  {typeof element === "object" ? (
+                    <>{renderSchema(element)}</>
+                  ) : (
+                    <Paper elevation={5} sx={{ height: "30px", width: "200px", padding: "10px" }}>
+                      <div style={{ display: "flex", flexDirection: "row", alignItems: "flex-start" }}>
+                        NOT
+                        <input type="checkbox" /* defaultChecked={checked} onChange={() => setChecked(!checked)} */ />
+                        <input style={{ fontSize: "19px", padding: "2px 5px", width: "140px" }} />
+                      </div>
+                    </Paper>
+                  )}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </Paper>
+    );
+  };
   return (
     <Grid container spacing={2} sx={{ background: "#e2e2e2" }}>
       <Grid item xs={6}>
@@ -152,10 +173,15 @@ export const SchemaGeneration = ({ }) => {
             <Select
               id="demo-simple-select-helper"
               value={selectedPassage && selectedPassage?.title ? selectedPassage?.title : passages[0]?.title}
-              onChange={handlePassageChange}>
+              onChange={handlePassageChange}
+            >
               {passages &&
                 passages?.length > 0 &&
-                passages?.map(doc => <MenuItem key={doc?.id} value={doc?.title}>{doc?.title}</MenuItem>)}
+                passages?.map(doc => (
+                  <MenuItem key={doc?.id} value={doc?.title}>
+                    {doc?.title}
+                  </MenuItem>
+                ))}
             </Select>
           </Box>
           <Box sx={{ display: "flex", flexDirection: "column", marginRight: "20px" }}>
@@ -167,10 +193,8 @@ export const SchemaGeneration = ({ }) => {
               id="key-phrase"
               value={selectedPhrase}
               options={phrases}
-              onChange={(_, value) => handlePhraseChange(value || null)}
-              renderInput={params => (
-                <TextField {...params} value={selectedPhrase} />
-              )}
+              onChange={(_, value) => setSelectedPhrase(value || null)}
+              renderInput={params => <TextField {...params} value={selectedPhrase} />}
               getOptionLabel={phrase => (phrase ? phrase : "")}
               renderOption={(props, phrase) => (
                 <li key={phrase} {...props}>
@@ -182,100 +206,10 @@ export const SchemaGeneration = ({ }) => {
               sx={{ mb: "16px" }}
             />
           </Box>
+          <>{renderSchema(schema)}</>
 
-          <div>
-            <button onClick={onAddAnd}>and</button>
-            <button onClick={onAddOR}>or</button>
-            <button onClick={onAddNot}>not</button>
-
-            {temp_schema.map((op, index) => {
-              if (op.combinator === "OR" || op.combinator === "AND") {
-                return renderOperators({ op, not: false });
-              } else if (op.combinator === "NOT") {
-                return renderOperators({
-                  not: true,
-                  op: {
-                    combinator: op.operator,
-                    keyWords: op.keyWords
-                  }
-                });
-              }
-              return null;
-            })}
-
-            <div>
-              {schema.ors.map((x, index) => (
-                <>
-                  <input value={x} />
-                  {schema.ors.length !== (index + 1) && <span>or</span>}
-                </>
-              ))}
-              {/* <input /> */}
-            </div>
-
-            <div>
-              {schema.ands.map((x, index) => (
-                <>
-                  <input value={x} />
-                  {schema.ands.length !== (index + 1) && <span>and</span>}
-                </>
-              ))}
-              {/* <input /> */}
-            </div>
-
-            <div>
-              <span>not</span>
-              {schema.nots.map((x, index) => (
-                <>
-                  <input value={x} />
-                  {schema.nots.length !== (index + 1) && <span>or</span>}
-                </>
-              ))}
-              {/* <input /> */}
-            </div>
-
-
-            <div>
-              {/* <Button
-                sx={{ borderRadius: 10 }}
-                variant="contained"
-                startIcon={<SaveIcon />}
-              >
-                Save
-              </Button> */}
-              {/* <IconButton size="large"> */}
-              <ArrowLeftIcon style={{ fontSize: '6.1875rem', color: '4DA14C' }} />
-              <ArrowRightIcon style={{ fontSize: '6.1875rem', color: '4DA14C' }} />
-
-              {/* </IconButton> */}
-            </div>
-
-            {/* <div>
-              <input />
-              <span>and</span>
-              <input />
-            </div>
-
-            <div>
-              <span>not</span>
-              <input />
-              <span>or</span>
-              <input />
-            </div>
-
-            <div>
-              <a className="more-skewed-right" href="#">
-                <span>MORE LONGER TEXT</span>
-              </a>
-            </div>
-
-            <div>
-              <a className="more-skewed-left" href="#">
-                <span>MORE LONGER TEXT</span>
-              </a>
-            </div> */}
-
-          </div>
+          <Button>Submit</Button>
+          <Button>Search</Button>
         </Item>
       </Grid>
       <Grid item xs={6}>
