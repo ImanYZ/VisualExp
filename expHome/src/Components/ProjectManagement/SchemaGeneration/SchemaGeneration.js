c91fc189bc5e3d5dc500aa2dd1583e5d42bf7ce8
Sam ouhra <ouhrac@gmail.com>
2022-10-06 10:50:30 +0100

    change the structure of the database and the design of the boolean structure

--- expHome/src/Components/ProjectManagement/SchemaGeneration/SchemaGeneration.js
+++ expHome/src/Components/ProjectManagement/SchemaGeneration/SchemaGeneration.js
@@ -1,53 +1,62 @@
 import React, { useEffect, useState } from "react";
 import { useRecoilValue } from "recoil";
-import { firebaseState, fullnameState } from "../../../store/AuthAtoms";
+import { firebaseState, fullnameState, emailState } from "../../../store/AuthAtoms";
 
 // mui imports
 import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Select from "@mui/material/Select";
-import { alpha, styled } from "@mui/material/styles";
+import { styled } from "@mui/material/styles";
 import Typography from "@mui/material/Typography";
 import MenuItem from "@mui/material/MenuItem";
 import Button from "@mui/material/Button";
-import SaveIcon from "@mui/icons-material/Save";
-import DeleteIcon from "@mui/icons-material/Delete";
-import ArrowRightIcon from "@mui/icons-material/ArrowRight";
-import ArrowLeftIcon from "@mui/icons-material/ArrowLeft";
 import IconButton from "@mui/material/IconButton";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
-import TreeView from "@mui/lab/TreeView";
-import TreeItem, { treeItemClasses } from "@mui/lab/TreeItem";
-import PropTypes from "prop-types";
-// import { useSpring, animated } from "react-spring/web.cjs";
-import Collapse from "@mui/material/Collapse";
-import SvgIcon from "@mui/material/SvgIcon";
-import AddIcon from "@mui/icons-material/Add";
+
 import "./SchemaGeneration.css";
-import Checkbox from "@mui/material/Checkbox";
+
+import QueryBuilder from "./QueryBuilder";
+import QuerySearch from "./QuerySearch";
 
 const temp_schema = {
-  id:"1",
+  id: "g-0.6224544849727727",
   combinator: "AND",
-  negations: [true, false],
-  keyWords: [
+  rules: [
     {
-      id:"2",
+      id: "g-0.3939927960314584",
       combinator: "OR",
-      negations: [true, false],
-      keyWords: ["skull", "crani"]
+      rules: [
+        {
+          id: "r-0.569942811653946",
+          combinator: "AND",
+          rules: [
+            {
+              id: "r-0.8457265715736604",
+              not: false,
+              key: "prey"
+            }
+          ]
+        }
+      ]
     },
     {
-      id:"3",
-      combinator: "OR",
-      negations: [true, false],
-      keyWords: ["skull", "crani"]
+      id: "r-0.84572657157304",
+      not: true,
+      key: "fac"
     },
-    "prey",
-    "mouse"
-  ],
+    {
+      id: "r-0.8457265715736604",
+      not: false,
+      key: "prey"
+    },
+    {
+      id: "r-0.84572655736604",
+      not: false,
+      key: "mouse"
+    }
+  ]
 };
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -65,8 +74,13 @@ export const SchemaGeneration = ({}) => {
   const [phrases, setSelectedPhrases] = useState([]);
   const [selectedPassage, setSelectedPassage] = useState({});
   const [selectedPhrase, setSelectedPhrase] = useState(null);
+  const [selectedPhrase1, setSelectedPhrase1] = useState(null);
   const [schema, setSchema] = useState(temp_schema);
-  const [select, setSelect] = useState("AND");
+  const email = useRecoilValue(emailState);
+  const [schemasBoolean, setSchemasBoolean] = useState([]);
+  const [schmaChanges, setSchmaChanges] = useState([]);
+  const [schmaLoadedUse, setSchmaLoadedUse] = useState(false);
+
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(async () => {
     const passagesDocs = await firebase.db.collection("passages").get();
@@ -83,6 +97,7 @@ export const SchemaGeneration = ({}) => {
       .filter(x => x !== null);
     // console.log({ passages });
     setPassages(passages);
+    console.log(passages);
     setSelectedPassage(passages[0]);
     const phrases = passages[0].phrases;
     setSelectedPhrases([...phrases]);
@@ -99,68 +114,155 @@ export const SchemaGeneration = ({}) => {
     setSelectedPhrase(passage.phrases[0]);
   };
 
-  const handleChange = event => {
-    setSelect(event.target.value);
-  };
+  useEffect(() => {
+    if (firebase && selectedPhrase) {
+      const schmaQuery = firebase.db.collection("booleanScratch").where("phrase", "==", selectedPhrase);
+      const schmaSnapshot = schmaQuery.onSnapshot(snapshot => {
+        const docChanges = snapshot.docChanges();
+        setSchmaChanges(oldSchmasChanges => {
+          return [...oldSchmasChanges, ...docChanges];
+        });
+        setSchmaLoadedUse(true);
+      });
+      return () => {
+        setSchmaChanges([]);
+        schmaSnapshot();
+      };
+    }
+  }, [firebase, selectedPhrase]);
 
-  const addRule = () => {
-    const tmpSchma = { ...schema };
-    tmpSchma.keyWords.push({
-      combinator: "OR",
-      negations: [true, false],
-      keyWords: ["skull", "crani"]
-    });
-    setSchema(tmpSchma);
-  };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(async () => {
+    let schemas = [...schemasBoolean];
+    if (!(selectedPhrase === selectedPhrase1)) {
+      schemas = [];
+    }
 
-  const addKeyword = () => {
-    const tmpSchma = { ...schema };
-    tmpSchma.keyWords.push("key");
-    setSchema(tmpSchma);
-    console.log(tmpSchma);
-  };
+    const tempSchemaChanges = [...schmaChanges];
+    setSchmaChanges([]);
 
-  const renderSchema = (tempSchema) => {
-    return (
-      <Paper elevation={3} sx={{ minheight: "300px", width: "97%", padding: "10px" }}>
-        <div style={{ flexDirection: "row", marginLeft: "500px", alignItems: "flex-start" }}>
-          <Button onClick={addKeyword}>ADD A KEYWORD</Button>
-          <Button onClick={addRule}>ADD A RULE</Button>
-        </div>
-        <div class="clt">
-          <ul>
-            <li>
-              <Box sx={{ marginRight: "890px" }}>
-                <div style={{ padding: "0px" }}>
-                  <select value={select} onChange={handleChange}>
-                    <option value="AND">AND</option>
-                    <option value="OR">OR</option>
-                  </select>
-                </div>
-              </Box>
-            </li>
-            {tempSchema?.keyWords?.map(element => {
-              return (
-                <li>
-                  {typeof element === "object" ? (
-                    <>{renderSchema(element)}</>
-                  ) : (
-                    <Paper elevation={5} sx={{ height: "30px", width: "200px", padding: "10px" }}>
-                      <div style={{ display: "flex", flexDirection: "row", alignItems: "flex-start" }}>
-                        NOT
-                        <input type="checkbox" /* defaultChecked={checked} onChange={() => setChecked(!checked)} */ />
-                        <input style={{ fontSize: "19px", padding: "2px 5px", width: "140px" }} />
-                      </div>
-                    </Paper>
-                  )}
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      </Paper>
-    );
+    for (let change of tempSchemaChanges) {
+      const shemaData = change.doc.data();
+      console.log(change.type);
+
+      if (change.type === "added") {
+        schemas.push({ id: change.doc.id, ...shemaData });
+        console.log(shemaData);
+      } else if (change.type === "modified") {
+        const index = schemas.indexOf(elm => elm.id === change.doc.id);
+        schemas[index] = { id: change.doc.id, ...shemaData };
+      }
+    }
+    console.log("schemas", schemas);
+    setSchemasBoolean(schemas);
+    setSchmaLoadedUse(false);
+    setSelectedPhrase1(selectedPhrase);
+  }, [firebase, schmaLoadedUse]);
+
+  const handleSubmit = () => {
+    console.log({ schema });
+    const newbooleanScratch = {
+      email,
+      fullname,
+      schema: schema,
+      createdAt: new Date(),
+      phrase: selectedPhrase,
+      passage: selectedPassage.title,
+      upVotes: 0,
+      downVotes: 0,
+      upVoters: [],
+      downVoters: []
+    };
+    const schemaGenerationRef = firebase.db.collection("booleanScratch").doc();
+    schemaGenerationRef
+      .set(newbooleanScratch)
+      .then(() => {
+        console.log("Document successfully written!");
+      })
+      .catch(error => {
+        console.error("Error writing document: ", error);
+      });
   };
+  const upVote = async schema => {
+    try {
+      const schemaGenerationRef = firebase.db.collection("booleanScratch").doc(schema.id);
+      const _upVoters = [...schema.upVoters];
+      const _downVoters = [...schema.downVoters];
+
+      let _upVotes = schema.upVotes;
+      let _downVotes = schema.downVotes;
+      if (_upVoters.includes(fullname)) {
+        _upVoters.splice(_upVoters.indexOf(fullname), 1);
+        _upVotes = _upVotes - 1;
+      } else {
+        _upVoters.push(fullname);
+        _upVotes = _upVotes + 1;
+        if (_downVoters.includes(fullname)) {
+          _downVoters.splice(_downVoters.indexOf(fullname), 1);
+          _downVotes = _downVotes - 1;
+        }
+      }
+      const schemaGenerationUpdate = {
+        upVotes: _upVotes,
+        upVoters: _upVoters,
+        downVotes: _downVotes,
+        downVoters: _downVoters
+      };
+      await schemaGenerationRef.update(schemaGenerationUpdate);
+      const index = schemasBoolean.findIndex(elt => elt.id === schema.id);
+      const _schemasBoolean = [...schemasBoolean];
+      _schemasBoolean[index] = {
+        ..._schemasBoolean[index],
+        upVotes: _upVotes,
+        upVoters: _upVoters,
+        downVotes: _downVotes,
+        downVoters: _downVoters
+      };
+      setSchemasBoolean(_schemasBoolean);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const downVote = async schema => {
+    try {
+      const schemaGenerationRef = firebase.db.collection("booleanScratch").doc(schema.id);
+      const _downVoters = [...schema.downVoters];
+      let _downVotes = schema.downVotes;
+      const _upVoters = [...schema.upVoters];
+      let _upVotes = schema.upVotes;
+      if (_downVoters.includes(fullname)) {
+        _downVoters.splice(_downVoters.indexOf(fullname), 1);
+        _downVotes = _downVotes - 1;
+      } else {
+        _downVoters.push(fullname);
+        _downVotes = _downVotes + 1;
+        if (_upVoters.includes(fullname)) {
+          _upVoters.splice(_upVoters.indexOf(fullname), 1);
+          _upVotes = _upVotes - 1;
+        }
+      }
+      const schemaGenerationUpdate = {
+        upVotes: _upVotes,
+        upVoters: _upVoters,
+        downVotes: _downVotes,
+        downVoters: _downVoters
+      };
+      await schemaGenerationRef.update(schemaGenerationUpdate);
+      const index = schemasBoolean.findIndex(elt => elt.id === schema.id);
+      const _schemasBoolean = [...schemasBoolean];
+      _schemasBoolean[index] = {
+        ..._schemasBoolean[index],
+        upVotes: _upVotes,
+        upVoters: _upVoters,
+        downVotes: _downVotes,
+        downVoters: _downVoters
+      };
+      setSchemasBoolean(_schemasBoolean);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  console.log(schemasBoolean);
   return (
     <Grid container spacing={2} sx={{ background: "#e2e2e2" }}>
       <Grid item xs={6}>
@@ -169,7 +271,6 @@ export const SchemaGeneration = ({}) => {
             <Typography variant="h6" component="div" align="left">
               Passage
             </Typography>
-
             <Select
               id="demo-simple-select-helper"
               value={selectedPassage && selectedPassage?.title ? selectedPassage?.title : passages[0]?.title}
@@ -206,14 +307,66 @@ export const SchemaGeneration = ({}) => {
               sx={{ mb: "16px" }}
             />
           </Box>
-          <>{renderSchema(schema)}</>
+          <QueryBuilder query={schema} onQueryChange={q => setSchema(q)} />
 
-          <Button>Submit</Button>
-          <Button>Search</Button>
+          <div
+            style={{
+              display: "flex",
+              width: "80%",
+              paddingTop: "10px",
+              paddingBottom: "20px",
+              justifyContent: "space-between"
+            }}
+          >
+            <Button variant="contained" onClick={handleSubmit}>
+              Submit
+            </Button>
+            <Button variant="contained" onClick={handleSubmit}>{`Search >>`}</Button>
+          </div>
+          {schemasBoolean?.length > 0 && (
+            <Typography variant="h6" component="div" align="left">
+              Previous Proposals:
+            </Typography>
+          )}
+
+          <Paper sx={{ height: "700px", overflow: "scroll" }}>
+            {schemasBoolean?.length > 0 &&
+              schemasBoolean.map(schemaE => {
+                return (
+                  <div style={{ height: "95%" }}>
+                    <QueryBuilder query={schemaE.schema} />
+                    <div style={{ display: "flex", width: "80%", justifyContent: "space-between" }}>
+                      <div style={{ display: "flex", justifyContent: "space-between" }}>
+                        <IconButton
+                          onClick={() => {
+                            upVote(schemaE);
+                          }}
+                          size="small"
+                        >
+                          {schemaE.upVotes}👍
+                        </IconButton>
+                        <IconButton
+                          onClick={() => {
+                            downVote(schemaE);
+                          }}
+                          size="small"
+                          style={{ color: "green" }}
+                        >
+                          {schemaE.downVotes}👎
+                        </IconButton>
+                      </div>
+
+                      <Button onClick={handleSubmit}>{`Search >>`}</Button>
+                    </div>
+                  </div>
+                );
+              })}
+          </Paper>
         </Item>
       </Grid>
       <Grid item xs={6}>
-        <Item>xs=4</Item>
+        <Item>We found that schema in the followings:</Item>
+        {/* <QuerySearch /> */}
       </Grid>
     </Grid>
   );
