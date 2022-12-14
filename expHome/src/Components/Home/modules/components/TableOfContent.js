049a9bbee84bc642246fa029c84005dec79394c1
elVengador <dev.jimynicanor@gmail.com>
2022-12-14 17:22:27 -0500

    advance adding TOC

--- expHome/src/Components/Home/modules/components/TableOfContent.js
+++ expHome/src/Components/Home/modules/components/TableOfContent.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 export const TableOfContent = ({ menuItems, onChangeContent }) => {
   return (
-    <Box component={"nav"} sx={{ position: "sticky", top: "200px" }} style={{ mixBlendMode: "difference" }}>
+    <Box component={"nav"} sx={{ position: "sticky", top: "200px", }} style={{ mixBlendMode: "difference" }}>
       <Box
         component={"ul"}
         sx={{
@@ -11,24 +11,39 @@ export const TableOfContent = ({ menuItems, onChangeContent }) => {
           flexDirection: "column",
           alignItems: "start",
           listStyle: "none",
-          padding: 0
+          padding: 0,
+          pl: "20px"
         }}
       >
         {menuItems.map((item) => (
           <Box component={"ul"}
             key={item.id}
             sx={{
+              color: item.active ? "#f1f1f1" : "#666666",
               display: "flex",
               flexDirection: "column",
               alignItems: "flex-start",
               listStyle: "none",
+              borderLeft: "solid 1px #666666",
               px: "10px",
+              position: "relative",
+              "&::after": {
+                content: '""',
+                width: "7px",
+                height: "7px",
+                borderRadius: "50%",
+                position: "absolute",
+                left: "-4px",
+                top: "26px",
+                background: item.active ? "#f1f1f1" : "#666666",
+                // border: "solid 1px #666666",
+              }
             }}
           >
             <Typography sx={{
-              color: "white",
               fontSize: "16px",
-              fontWeight: "700",
+              // fontWeight: "700",
+              py: "20px",
               cursor: "pointer",
               ":hover": {
                 color: "orange"
@@ -36,21 +51,24 @@ export const TableOfContent = ({ menuItems, onChangeContent }) => {
             }}>
               {item.title}
             </Typography>
-            {item.children.length && (
+            {item.children.length > 0 && (
               <Box
                 component={"ul"}
                 sx={{
                   display: "flex",
                   flexDirection: "column",
                   alignItems: "flex-start",
-                  listStyle: "outside",
-
+                  listStyle: "none",
+                  pl: "15px"
                 }}
               >
                 {item.children.map((child) => (
                   <Box component={"li"} key={child.id} onClick={() => onChangeContent(child.target)}>
                     <Typography sx={{
-                      fontSize: "16px", cursor: "pointer", ":hover": {
+                      fontSize: "16px",
+                      cursor: "pointer",
+                      py: "10px",
+                      ":hover": {
                         color: "orange"
                       },
                     }}>
