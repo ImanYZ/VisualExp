a4227b0a582bfdd456e04ee080eee637b3613280
elVengador <dev.jimynicanor@gmail.com>
2022-12-14 10:29:14 -0500

    advane addin animation

--- expHome/src/Components/Home/Home.js
+++ expHome/src/Components/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useMemo, useRef, useState } from "react";
 
 import Box from "@mui/material/Box";
 
@@ -14,14 +14,146 @@ import withRoot from "./modules/withRoot";
 
 import sectionsOrder from "./modules/views/sectionsOrder";
 import UniversitiesMap from "./modules/views/UniversitiesMap/UniversitiesMap";
+import { useRive } from "@rive-app/react-canvas";
+
+const artboards = [
+  { name: "animation1", durationMs: 5000 },
+  { name: "animation2", durationMs: 30000 },
+  { name: "animation3", durationMs: 8000 },
+  { name: "animation4", durationMs: 24000 },
+  { name: "animation5", durationMs: 13000 },
+  { name: "animation6", durationMs: 3000 }
+]
 
 function Index() {
   const [section, setSection] = useState(0);
   const [notSectionSwitching, setNotSectionSwitching] = useState(true);
 
+  const { rive, RiveComponent } = useRive({
+    src: "gg.riv",
+    stateMachines: artboards[0].name,
+    autoplay: false,
+  });
+
   const step0Ref = useRef(null);
+  const sectionAnimationControllerRef = useRef(null)
+  const section1Ref = useRef(null)
+  const section2Ref = useRef(null)
+  const section3Ref = useRef(null)
+  const section4Ref = useRef(null)
+  const section5Ref = useRef(null)
+  const section6Ref = useRef(null)
+
+  const onScrollAnimation = (target) => {
+    const scrollOffset = target.scrollTop
+    const scrollTotal = target.scrollHeight - window.innerHeight
+    const percentage = 100 * scrollOffset / scrollTotal
+    // setScrollPercentage(percentage)
+
+    if (!section1Ref.current) return
+    if (!section2Ref.current) return
+    if (!section3Ref.current) return
+    if (!section4Ref.current) return
+    if (!section5Ref.current) return
+    if (!section6Ref.current) return
+    // const pageHeight = target.scrollHeight
+    const section1Height = section1Ref.current.clientHeight
+    const section2Height = section2Ref.current.clientHeight
+    const section3Height = section3Ref.current.clientHeight
+    const section4Height = section4Ref.current.clientHeight
+    const section5Height = section5Ref.current.clientHeight
+    const section6Height = section6Ref.current.clientHeight
+
+    const sectionsHeight = [section1Height, section2Height, section3Height, section4Height, section5Height, section6Height]
+    const animationSectionIndex = 1
+
+    const { index, cumulativeHeight } = sectionsHeight.reduce((acu, cur, idx) => {
+      if (acu.index >= 0) return acu
+      const newCumulativeHeight = acu.cumulativeHeight + cur
+      if (scrollOffset < newCumulativeHeight) return { cumulativeHeight: newCumulativeHeight, index: idx }
+      return { ...acu, cumulativeHeight: newCumulativeHeight }
+    }, { cumulativeHeight: 0, index: -1 })
+
+    if (index < 0) return Error('cant detect current` section')
+
+    if (index < animationSectionIndex) {
+      // show first artboard and first frame
+      rive.reset({ artboard: artboards[0].name })
+      rive.scrub("Timeline 1", 0)
+    }
+    if (index > animationSectionIndex) {
+      // show last artboard and last frame
+      rive.reset({ artboard: artboards[artboards.length - 1].name })
+      rive.scrub("Timeline 1", artboards[artboards.length - 1].durationMs / 1000)
+    }
+    if (index === animationSectionIndex) {
+      console.log('Scroll in animation section')
+      // check local percentage
+      const lowerLimit = cumulativeHeight - sectionsHeight[animationSectionIndex]
+      const upperLimit = cumulativeHeight
+
+      const rangeAnimation = upperLimit - lowerLimit
+      const positionAnimation = scrollOffset - lowerLimit
+      const percentageAnimation = positionAnimation * 100 / rangeAnimation
+      console.log({ percentageAnimation })
+      // setAnimationScrollPercentage(percentageAnimation)
+
+      // get animation section selected
+      if (!sectionAnimationControllerRef.current) return
+
+      const animation1Height = sectionAnimationControllerRef.current.getAnimation1Height()
+      const animation2Height = sectionAnimationControllerRef.current.getAnimation2Height()
+      const animation3Height = sectionAnimationControllerRef.current.getAnimation3Height()
+      const animation4Height = sectionAnimationControllerRef.current.getAnimation4Height()
+      const animation5Height = sectionAnimationControllerRef.current.getAnimation5Height()
+      const animation6Height = sectionAnimationControllerRef.current.getAnimation6Height()
+
+      const animationsHeight = [animation1Height, animation2Height, animation3Height, animation4Height, animation5Height, animation6Height]
+
+      console.log({ lowerLimit })
+
+      const { indexAnimation, cumulativeAnimationHeight } = animationsHeight.reduce((acu, cur, idx) => {
+        if (acu.indexAnimation >= 0) return acu
+        const cumulativeAnimationHeight = acu.cumulativeAnimationHeight + cur
+        // console.log(1, { acu })
+        // console.log(2, '<', { scrollOffset, cumulativeAnimationHeight })
+        if (scrollOffset < cumulativeAnimationHeight) return { cumulativeAnimationHeight, indexAnimation: idx }
+        return { ...acu, cumulativeAnimationHeight }
+      }, { cumulativeAnimationHeight: lowerLimit, indexAnimation: -1 })
+
+      if (indexAnimation < 0) return Error('cant detect current animation')
+
+      console.log('--->', { indexAnimation, cumulativeAnimationHeight })
+
+      const lowerAnimationLimit = cumulativeAnimationHeight - animationsHeight[indexAnimation]
+      const upperAnimationLimit = cumulativeAnimationHeight
+
+      const rangeFrames = upperAnimationLimit - lowerAnimationLimit
+      const positionFrame = scrollOffset - lowerAnimationLimit
+      const percentageFrame = positionFrame * 100 / rangeFrames
+      console.log({ lowerAnimationLimit, upperAnimationLimit, rangeFrames, positionFrame })
+      console.log('percentageFrame', percentageFrame.toFixed(1))
+
+      rive.reset({ artboard: artboards[indexAnimation].name })
+      const timeInSeconds = artboards[indexAnimation].durationMs / 1000 * percentageFrame / 100
+      rive.scrub("Timeline 1", timeInSeconds)
+      // setFramePercentage(percentageFrame)
+    }
+
+  }
+
+  const RiveComponentMemo = useMemo(() => {
+    return <RiveComponent
+      // onMouseEnter={() => rive && rive.play()}
+      // onMouseLeave={() => rive && rive.pause()}
+      className="rive-canvas"
+    />
+  }, [])
 
   const updatePosition = (event) => {
+    console.log('updatePosition')
+    onScrollAnimation(event.currentTarget)
+
     if (notSectionSwitching) {
       let cumulativeHeight = 0;
       for (let sIdx = 0; sIdx < sectionsOrder.length; sIdx++) {
@@ -102,15 +234,25 @@ function Index() {
         joinUsClick={joinUsClick}
         thisPage={section === sectionsOrder.length - 2 ? "Apply!" : undefined}
       />
-      <Box id="step-0" ref={step0Ref}>
+      <Box id="step-0" ref={section1Ref}>
         <Landing />
       </Box>
-      <HowItWorks section={section} ref={step0Ref} />
-      <What />
-      <Values />
+      <Box ref={section2Ref}>
+        <HowItWorks section={section} riveComponent={RiveComponentMemo} ref={sectionAnimationControllerRef} />
+      </Box>
+      <Box ref={section3Ref}>
+        <What />
+      </Box>
+      <Box ref={section4Ref}>
+        <Values />
+      </Box>
       {/* <UniversitiesMap theme={"Light"} /> */}
-      <WhoWeAre />
-      <JoinUs />
+      <Box ref={section5Ref}>
+        <WhoWeAre />
+      </Box>
+      <Box ref={section6Ref}>
+        <JoinUs />
+      </Box>
       <AppFooter />
     </Box>
   );
