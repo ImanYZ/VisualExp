f1148a12243f9b7cc04f07400040040c10f34489
elVengador <dev.jimynicanor@gmail.com>
2022-12-16 12:31:11 -0500

    move why after animation

--- expHome/src/Components/Home/modules/views/HowItWorks.js
+++ expHome/src/Components/Home/modules/views/HowItWorks.js
@@ -70,58 +70,43 @@ const HowItWorks = (props) => {
         }}
       >
 
-        {/* <Box
-        component="img"
-        src="/static/CurvyLines.png"
-        alt="curvy lines"
-        sx={{
-          pointerEvents: "none",
-          position: "absolute",
-          top: -180,
-          opacity: 0.7,
-          zIndex: 0,
-        }}
-      /> */}
-
-
         {/* --- animations start */}
         <div style={{ position: 'sticky', top: topCenteredPosition, width: boxLarge, height: boxLarge, display: 'flex', flexDirection: 'column', zIndex: 10/* , border: "solid 2px pink" */ }}>
           {props.riveComponent}
         </div>
         <div ref={animation0Ref} style={{ height: boxLarge, width: boxLarge, position: "absolute"/* , background: "#3b5015" */, top: "0px", padding: "20px" }}>
-          <Typography variant="h4" marked="center" sx={{ /* mb: 7, */ color: "#f8f8f8", textAlign: "center",paddingTop:"56px" }}>
+          <Typography variant="h4" marked="center" sx={{ /* mb: 7, */ color: "#f8f8f8", textAlign: "center", paddingTop: "56px" }}>
             {sectionsOrder[sectionIdx].title}
           </Typography>
-          {/* <h2 style={{ color: 'orange', background: "black" }}>0: {animation0Ref?.current?.clientHeight ?? 0}</h2> */}
+
         </div>
         <div ref={animation1Ref} style={{ height: "1000vh", width: "100%"/* , borderRight: "solid 20px #8031a5" */ }}>
-          {/* <h2 style={{ color: 'orange', background: "black" }}>1: {animation1Ref?.current?.clientHeight ?? 0}</h2> */}
+
         </div>
         <div ref={animation2Ref} style={{ height: "500vh", width: "100%"/* , borderRight: "solid 20px #2769aa" */ }}>
-          {/* <h2 style={{ color: 'orange', background: "black" }}>2: {animation2Ref?.current?.clientHeight ?? 0}</h2> */}
+
         </div>
         <div ref={animation3Ref} style={{ height: "300vh", width: "100%"/* , borderRight: "solid 20px #3696f7" */ }}>
-          {/* <h2 style={{ color: 'orange', background: "black" }}>3: {animation3Ref?.current?.clientHeight ?? 0}</h2> */}
+
         </div>
         <div ref={animation4Ref} style={{ height: "300vh", width: "100%"/* , borderRight: "solid 20px #26c2ff" */ }}>
-          {/* <h2 style={{ color: 'orange', background: "black" }}>4: {animation4Ref?.current?.clientHeight ?? 0}</h2> */}
+
         </div>
         <div ref={animation5Ref} style={{ height: "300vh", width: "100%"/* , borderRight: "solid 20px #24f0ff" */ }}>
-          {/* <h2 style={{ color: 'orange', background: "black" }}>5: {animation5Ref?.current?.clientHeight ?? 0}</h2> */}
+
         </div>
-        {/* <div ref={animation6Ref} style={{ height: "100vh", width: "100%", borderRight: "solid 20px #15e9a2", position: "absolute", bottom: "0px", left: "0px" }}>6</div> */}
 
         {/* --- animation ends */}
         <Button
-        color="secondary"
-        variant="contained"
-        size="large"
-        component="a"
-        href="#JoinUsSection"
-        sx={{ minWidth: 200, color: "common.white" ,zIndex:"10"}}
-      >
-        Apply to Join Us!
-      </Button>
+          color="secondary"
+          variant="contained"
+          size="large"
+          component="a"
+          href="#JoinUsSection"
+          sx={{ minWidth: 200, color: "common.white", zIndex: "10" }}
+        >
+          Apply to Join Us!
+        </Button>
         <Box sx={{ zIndex: 1, mx: "auto" }}>
           {/* <Grid container spacing={2.5} align="center">
             {howElements.map((elem, idx) => {
