e31bbaeff0cf0f2c2bf9da9551e1d6974553dccc
elVengador <dev.jimynicanor@gmail.com>
2022-12-16 18:11:22 -0500

    fix titles

--- expHome/src/Components/Home/modules/views/sectionsOrder.js
+++ expHome/src/Components/Home/modules/views/sectionsOrder.js
@@ -1,16 +1,16 @@
 export default [
   { id: "LandingSection", label: "Landing", title: "1Cademy's Landing Page" },
-  { id: "HowItWorksSection", label: "How", title: "How We Work" },
-  { id: "ValuesSection", label: "Why", title: "Why 1Cademy Helps" },
+  { id: "HowItWorksSection", label: "How", title: "How We Work?" },
+  { id: "ValuesSection", label: "Why", title: "Why 1Cademy?" },
   {
     id: "CommunitiesSection",
     label: "What",
-    title: "Our Communities",
+    title: "What we study?",
   },
   {
     id: "SchoolsSection",
     label: "Where",
-    title: "Where Are We From?",
+    title: "Where Are We?",
   },
   { id: "WhoWeAreSection", label: "Who", title: "Who Is Behind 1Cademy?" },
   { id: "JoinUsSection", label: "Apply", title: "Apply to Join Us!" },
