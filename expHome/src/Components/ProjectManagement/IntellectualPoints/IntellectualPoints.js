5994e1dd508997b3b4a828a8a549e03e546e7fd4
Sam ouhra <ouhrac@gmail.com>
2022-12-19 14:01:54 +0100

    fix the issue with not being able to  see the place to enter my intellectual activity times in 1Cademy or submit my activities.

--- expHome/src/Components/ProjectManagement/IntellectualPoints/IntellectualPoints.js
+++ expHome/src/Components/ProjectManagement/IntellectualPoints/IntellectualPoints.js
@@ -15,10 +15,10 @@ import CircularProgress from "@mui/material/CircularProgress";
 
 import { DataGrid } from "@mui/x-data-grid";
 
-import LocalizationProvider from "@mui/lab/LocalizationProvider";
-import AdapterDateFns from "@mui/lab/AdapterDateFns";
-import DatePicker from "@mui/lab/DatePicker";
-import TimePicker from "@mui/lab/TimePicker";
+import {LocalizationProvider} from "@mui/x-date-pickers/LocalizationProvider";
+import {AdapterDateFns} from "@mui/x-date-pickers/AdapterDateFns";
+import {DatePicker} from "@mui/x-date-pickers/DatePicker";
+import {TimePicker} from "@mui/x-date-pickers/TimePicker";
 
 import {
   firebaseState,
