8495dd6d73900ad85d11988a68c70de68003e05e
Ameer Hamza <r3alst@gmail.com>
2022-12-23 20:53:00 +0500

    Sign up api for new auth flow

---
+++ expHome/functions/jest.config.js
@@ -0,0 +1,13 @@
+const cors = require("cors");
+
+require("dotenv").config({
+  path: "./.env.test"
+});
+
+/** @type {import('jest').Config} */
+const config = {
+  verbose: true,
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)"],
+};
+
+module.exports = config;
