a397d916b469548973d09fa90573037501ae7e4c
elVengador <dev.jimynicanor@gmail.com>
2022-12-28 11:34:54 -0500

    fix who section wrapper

---
+++ expHome/src/Components/Home/modules/views/WhoWeAreWrapper.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import WhoWeAre from './WhoWeAre'
+
+const WhoWeAreWrapper = (props) => {
+  return (
+    <WhoWeAre {...props} />
+  )
+}
+
+export default WhoWeAreWrapper
