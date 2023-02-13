8390d15b0eee02dadab8ddb26023f8fe7c1da798
Sam ouhra <ouhrac@gmail.com>
2023-02-13 21:21:40 +0100

    remove unnecessary loop from the participant.js logic

--- expHome/functions/api/participants.js
+++ expHome/functions/api/participants.js
@@ -71,16 +71,13 @@ participantsRouter.post("/schedule", async (req, res) => {
       for(const researcher in resScheduled) {
         for(const participant in resScheduled[researcher]) {
           const __scheduled = Object.values(resScheduled[researcher][participant]);
-          for(const _scheduled of __scheduled) {
-
-            for(const scheduledSlot of _scheduled) {
-              if(!availableScheduleByResearchers[scheduledSlot] || participant === fullname ) {
-                continue;
-              }
-              if(availableScheduleByResearchers[scheduledSlot].includes(researcher)) {
-                const idx = availableScheduleByResearchers[scheduledSlot].indexOf(researcher);
-                availableScheduleByResearchers[scheduledSlot].splice(idx, 1);
-              }
+          for (const scheduledSlot of __scheduled) {
+            if (!availableScheduleByResearchers[scheduledSlot] || participant === fullname) {
+              continue;
+            }
+            if (availableScheduleByResearchers[scheduledSlot].includes(researcher)) {
+              const idx = availableScheduleByResearchers[scheduledSlot].indexOf(researcher);
+              availableScheduleByResearchers[scheduledSlot].splice(idx, 1);
             }
           }
         }
