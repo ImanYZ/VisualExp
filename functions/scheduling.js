5e342eca24aa2dd19c5564490ed3a34efea37874
Sam ouhra <ouhrac@gmail.com>
2023-06-09 04:39:03 +0100

    fix issues marking the participant as attended

--- functions/scheduling.js
+++ functions/scheduling.js
@@ -253,11 +253,7 @@ exports.getOngoingResearcherEvent = async (req, res) => {
       const scheduleData = schedule.docs[0].data();
       const participantEmail = scheduleData.email;
 
-      let userDocs = await db.collection("instructors").where("email", "==", participantEmail.toLowerCase()).get();
-
-      if (userDocs.docs.length === 0) {
-        userDocs = await db.collection("users").where("email", "==", participantEmail.toLowerCase()).get();
-      }
+      let userDocs = await db.collection("users").where("email", "==", participantEmail.toLowerCase()).get();
 
       if (userDocs.docs.length === 0) {
         userDocs = await db.collection("usersSurvey").where("email", "==", participantEmail.toLowerCase()).get();
@@ -296,48 +292,70 @@ exports.getOngoingResearcherEvent = async (req, res) => {
 
 exports.markAttended = async (req, res) => {
   try {
-    const { ev, fullname } = req.body;
-    const { scheduleId, project } = ev.schedule;
-    const participantFullname = ev.schedule.userId;
-    const order = ev.schedule.order;
-    const session = ev.schedule.session;
-    const month = moment(session).utcOffset(-4).startOf("month").format("YYYY-MM-DD");
-    const resScheduleDocs = await db
-      .collection("resSchedule")
-      .where("month", "==", month)
-      .where("project", "==", project)
-      .get();
-    let resScheduleData = {};
-    if (resScheduleDocs.docs.length > 0) {
-      resScheduleData = resScheduleDocs.docs[0].data();
-      if (!resScheduleData.hasOwnProperty("attendedSessions")) {
-        resScheduleData.attendedSessions = {};
-      }
-      const attendedSessions = resScheduleData.attendedSessions;
-      if (!attendedSessions.hasOwnProperty(fullname)) {
-        attendedSessions[fullname] = {};
-      }
-      if (attendedSessions.hasOwnProperty(fullname)) {
-        const startedSessionsByUser = attendedSessions[fullname];
-        if (startedSessionsByUser.hasOwnProperty(participantFullname)) {
-          if (!startedSessionsByUser[participantFullname].includes(order)) {
-            startedSessionsByUser[participantFullname].push(order);
+    await db.runTransaction(async t => {
+      const { ev, fullname, email } = req.body;
+      const { scheduleId, project } = ev.schedule;
+      const participantFullname = ev.schedule.userId;
+      const order = ev.schedule.order;
+      const session = ev.schedule.session;
+      const month = moment(session).utcOffset(-4).startOf("month").format("YYYY-MM-DD");
+      const resScheduleDocs = await t.get(
+        db.collection("resSchedule").where("month", "==", month).where("project", "==", project)
+      );
+      let resScheduleData = {};
+      if (resScheduleDocs.docs.length > 0) {
+        resScheduleData = resScheduleDocs.docs[0].data();
+        if (!resScheduleData.hasOwnProperty("attendedSessions")) {
+          resScheduleData.attendedSessions = {};
+        }
+        const attendedSessions = resScheduleData.attendedSessions;
+        if (!attendedSessions.hasOwnProperty(fullname)) {
+          attendedSessions[fullname] = {};
+        }
+        if (attendedSessions.hasOwnProperty(fullname)) {
+          const startedSessionsByUser = attendedSessions[fullname];
+          if (startedSessionsByUser.hasOwnProperty(participantFullname)) {
+            if (!startedSessionsByUser[participantFullname].includes(order)) {
+              startedSessionsByUser[participantFullname].push(order);
+            }
+          } else {
+            startedSessionsByUser[participantFullname] = [order];
           }
-        } else {
-          startedSessionsByUser[participantFullname] = [order];
         }
       }
-    }
-    const resScheduleRef = db.collection("resSchedule").doc(resScheduleDocs.docs[0].id);
-    await resScheduleRef.update({ attendedSessions: resScheduleData.attendedSessions });
-    await db.collection("schedule").doc(scheduleId).update({ attended: true });
-    console.log("project", project);
-    if (project === "OnlineCommunities") {
-      const userRef = db.collection("usersSurvey").doc(participantFullname);
-      await userRef.update({ projectDone: true });
-    }
+      const scheduleDoc = await t.get(db.collection("schedule").doc(scheduleId));
+      const scheduleData = scheduleDoc.data();
+      if (project === "OnlineCommunities") {
+        const resRef = db.collection("researchers").doc(fullname);
+        const resDoc = await t.get(resRef);
+        const resData = resDoc.data();
+        const userRef = db.collection("usersSurvey").doc(participantFullname);
+        t.update(userRef, { projectDone: true });
+        const expSessionRef = db.collection("expSessions").doc();
+        t.set(expSessionRef, {
+          attendees: [scheduleData.email, email],
+          createdAt: new Date(),
+          eTime: moment.tz(ev.event.end.dateTime, ev.event.end.timeZone).toDate(),
+          sTime: moment.tz(ev.event.start.dateTime, ev.event.start.timeZone).toDate(),
+          points: 10,
+          project: project,
+          eventId: scheduleData.id,
+          researcher: fullname,
+          user: participantFullname
+        });
+        if (resData.projects.hasOwnProperty(project)) {
+          resData.projects[project].expPoints = (resData.projects[project]?.expPoints || 0) + 10;
+          t.update(resRef, { projects: resData.projects });
+        }
+      }
+      t.update(db.collection("resSchedule").doc(resScheduleDocs.docs[0].id), {
+        attendedSessions: resScheduleData.attendedSessions
+      });
+      t.update(db.collection("schedule").doc(scheduleId), { attended: true });
+    });
     res.status(200).send({ message: "seccess" });
   } catch (error) {
+    console.log(error);
     res.status(500).send({ message: "Something went wrong", error });
   }
 };
