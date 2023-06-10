4d31dbf54f45f4683ae8977f5ea162a7df510590
Sam ouhra <ouhrac@gmail.com>
2023-06-10 19:13:09 +0100

    add endpoints to get the status of the entreview session and mark the participant as attended

--- functions/scheduling.js
+++ functions/scheduling.js
@@ -15,6 +15,7 @@ const {
 
 const { pad2Num, capitalizeFirstLetter } = require("./utils");
 const { toOrdinal } = require("number-to-words");
+const { findIndex } = require("lodash");
 
 const createExperimentEvent = async (email, researcher, order, start, end, projectSpecs, surveyType) => {
   const isAnnotating = projectSpecs.numberOfSessions === 1;
@@ -866,5 +867,105 @@ exports.retrieveEvents = async (req, res) => {
       evs.push(event);
     }
     res.status(200).json({ events: evs });
+  } catch (error) {}
+};
+
+exports.markEntreviewAttended = async (req, res) => {
+  try {
+    await db.runTransaction(async t => {
+      const meetingURL = req.body.meetingURL;
+      const researchersDocs = await t.get(db.collection("researchers"));
+      const researchersHash = {};
+      researchersDocs.forEach(researcherDoc => {
+        researchersHash[researcherDoc.data().email] = { fullname: researcherDoc.id, ...researcherDoc.data() };
+      });
+      const now = new Date().getTime();
+      const start = new Date(now - 60 * 60 * 1000);
+      let end = new Date(now + 60 * 60 * 1000);
+      const allEvents = await getEvents(start, end, "America/Detroit");
+      const eventIdx = allEvents.findIndex(ev => meetingURL.includes(ev.hangoutLink));
+      if (eventIdx === -1) {
+        return res.status(404).json({ message: "Event not found" });
+      }
+      const event = allEvents[eventIdx];
+      const scheduleDoc = await db.collection("schedule").where("id", "==", event.id).get();
+      if (scheduleDoc.docs.length > 0) {
+        const participant = scheduleDoc.docs[0].data().email;
+        const userDoc = await t.get(db.collection("usersSurvey").where("email", "==", participant));
+        const transcriptDoc = await t.get(db.collection("transcript").where("mettingUrl", "==", meetingURL));
+        const scheduleId = scheduleDoc.docs[0].id;
+        t.update(db.collection("schedule").doc(scheduleId), { started: true, attended: true });
+        if (userDoc.docs.length > 0 && transcriptDoc.docs.length === 0) {
+          const data = userDoc.docs[0].data();
+          const transcriptRef = db.collection("transcript").doc();
+          t.set(transcriptRef, {
+            mettingUrl: meetingURL,
+            participant: userDoc.docs[0].id,
+            surveyType: data.surveyType,
+            createdAt: new Date()
+          });
+          const userRef = db.collection("usersSurvey").doc(userDoc.docs[0].id);
+          t.update(userRef, { projectDone: true });
+          let researcher = "";
+          for (let attendee of event.attendees) {
+            if (
+              researchersHash.hasOwnProperty(attendee.email) &&
+              // attendee.email !== "ouhrac@gmail.com" &&
+              attendee.email !== "oneweb@umich.edu"
+            ) {
+              researcher = attendee.email;
+              break;
+            }
+          }
+          if (researcher !== "") { 
+            const resData = researchersHash[researcher];
+            const resRef = db.collection("researchers").doc(resData.fullname);
+            const expSessionRef = db.collection("expSessions").doc();
+            t.set(expSessionRef, {
+              attendees: [researcher, participant],
+              createdAt: new Date(),
+              eTime: moment.tz(event.end.dateTime, event.end.timeZone).toDate(),
+              sTime: moment.tz(event.start.dateTime, event.start.timeZone).toDate(),
+              points: 10,
+              project: "OnlineCommunities",
+              eventId: event.id,
+              researcher: resData.fullname,
+              user: userDoc.docs[0].id
+            });
+            if (resData.projects.hasOwnProperty("OnlineCommunities")) {
+              resData.projects["OnlineCommunities"].expPoints =
+                (resData.projects["OnlineCommunities"]?.expPoints || 0) + 10;
+              t.update(resRef, { projects: resData.projects });
+            }
+          }
+        }
+      }
+      res.status(200).json({ success: true });
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.checkEntreviewStatus = async (req, res) => {
+  try {
+    const now = new Date().getTime();
+    const start = new Date(now - 60 * 60 * 1000);
+    let end = new Date(now + 60 * 60 * 1000);
+    const allEvents = await getEvents(start, end, "America/Detroit");
+    const eventIdx = allEvents.findIndex(ev => req.body.meetingURL.includes(ev.hangoutLink));
+    if (eventIdx === -1) {
+      return res.status(200).json({ message: false, attended: false });
+    } else {
+      const scheduleDoc = await db.collection("schedule").where("id", "==", allEvents[eventIdx].id).get();
+      let attended = false;
+      if (scheduleDoc.docs.length > 0) {
+        const scheduledata = scheduleDoc.docs[0].data();
+        if (scheduledata.hasOwnProperty("attended")) {
+          attended = scheduledata.attended;
+        }
+      }
+      return res.status(200).json({ message: true, attended });
+    }
   } catch (error) {}
 };
