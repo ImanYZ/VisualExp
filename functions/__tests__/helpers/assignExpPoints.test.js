6d6814a2368426bb8c9d7207384a8d72f1f9fb9d
Sam ouhra <ouhrac@gmail.com>
2023-08-13 17:45:26 +0100

    fix unit tests

--- functions/__tests__/helpers/assignExpPoints.test.js
+++ functions/__tests__/helpers/assignExpPoints.test.js
@@ -1,4 +1,3 @@
-const moment = require("moment");
 const {
   MockData,
   mockProjectSpecs,
@@ -59,64 +58,76 @@ describe("assignExpPoints", () => {
     });
 
     it("researcher should get points for 1st session", async () => {
+      const researcherDoc = await db.collection("researchers").doc(researcher).get();
+      const researcherData = researcherDoc.data();
+      let researchersUpdates = {
+        [researcher]: researcherData
+      };
       await db.runTransaction(async t => {
         let transactionWrites = [];
         await assignExpPoints({
-          researcher,
+          researcher: { docId: researcher, ...researcherData },
           participant,
           session: "1st",
+          project,
           recallGradeData: _mockRecallGrades[0],
           feedbackCodeData: _mockFeedbackCodes[0],
-          project,
+          eventId: "",
           transactionWrites,
+          researchersUpdates,
           t
         });
         await writeTransaction(transactionWrites, t);
       });
-
-      const researcherDoc = await db.collection("researchers").doc(researcher).get();
-      const researcherData = researcherDoc.data();
-      expect(researcherData.projects[project].expPoints).toEqual(16);
+      expect(researchersUpdates[researcher].projects[project].expPoints).toEqual(16);
     });
 
     it("researcher should get points for 2nd session", async () => {
+      const researcherDoc = await db.collection("researchers").doc(researcher).get();
+      const researcherData = researcherDoc.data();
+      let researchersUpdates = {
+        [researcher]: researcherData
+      };
       await db.runTransaction(async t => {
         let transactionWrites = [];
-         await assignExpPoints({
-          researcher,
+        await assignExpPoints({
+          researcher: { docId: researcher, ...researcherData },
           participant,
           session: "2nd",
           recallGradeData: _mockRecallGrades[0],
           feedbackCodeData: _mockFeedbackCodes[0],
           project,
+          researchersUpdates,
           transactionWrites,
           t
         });
         await writeTransaction(transactionWrites, t);
       });
-      const researcherDoc = await db.collection("researchers").doc(researcher).get();
-      const researcherData = researcherDoc.data();
-      expect(researcherData.projects[project].expPoints).toEqual(10);
+      expect(researchersUpdates[researcher].projects[project].expPoints).toEqual(10);
     });
 
     it("researcher should get points for 3rd session", async () => {
+      const researcherDoc = await db.collection("researchers").doc(researcher).get();
+      const researcherData = researcherDoc.data();
+      let researchersUpdates = {
+        [researcher]: researcherData
+      };
       await db.runTransaction(async t => {
-      let transactionWrites = [];
-       await assignExpPoints({
-          researcher,
+        let transactionWrites = [];
+        await assignExpPoints({
+          researcher: { docId: researcher, ...researcherData },
           participant,
           session: "2nd",
           recallGradeData: _mockRecallGrades[0],
           feedbackCodeData: _mockFeedbackCodes[0],
           project,
+          researchersUpdates,
           transactionWrites,
           t
         });
         await writeTransaction(transactionWrites, t);
       });
-      const researcherDoc = await db.collection("researchers").doc(researcher).get();
-      const researcherData = researcherDoc.data();
-      expect(researcherData.projects[project].expPoints).toEqual(10);
+      expect(researchersUpdates[researcher].projects[project].expPoints).toEqual(10);
     });
   });
 
@@ -125,10 +136,14 @@ describe("assignExpPoints", () => {
     const participant = mockUsers.data[1].documentId;
     const project = mockUsers.data[1].project;
 
+    let _mockSchedules = [...mockSchedules.data];
+    _mockSchedules = _mockSchedules.map(schedule => ({ ...schedule, hasStarted: true }));
+
     const collects = [
       mockUsers,
       new MockData([], "expSessions"),
       new MockData([], "researcherLogs"),
+      new MockData(_mockSchedules, "schedule"),
       mockProjectSpecs,
       mockPassages,
       mockConditions,
@@ -145,42 +160,50 @@ describe("assignExpPoints", () => {
     });
 
     it("researcher should get points for taking session with survey participant", async () => {
+      const researcherDoc = await db.collection("researchers").doc(researcher).get();
+      const researcherData = researcherDoc.data();
+      let researchersUpdates = {
+        [researcher]: researcherData
+      };
       await db.runTransaction(async t => {
         let transactionWrites = [];
         await assignExpPoints({
-          researcher,
+          researcher: { docId: researcher, ...researcherData },
           participant,
           session: "1st",
           project,
           checkRecallgrading: false,
-          eventId: "1ktl3t9aer52is8oh1b90nuv54",
+          eventId: "",
+          researchersUpdates,
           transactionWrites,
           t
         });
         await writeTransaction(transactionWrites, t);
       });
-      const researcherDoc = await db.collection("researchers").doc(researcher).get();
-      const researcherData = researcherDoc.data();
-      expect(researcherData.projects[project].expPoints).toEqual(16);
+      expect(researchersUpdates[researcher].projects[project].expPoints).toEqual(16);
     });
 
     it("researcher should get points for taking session with survey participant (Annotating)", async () => {
+      const researcherDoc = await db.collection("researchers").doc(researcher).get();
+      const researcherData = researcherDoc.data();
+      let researchersUpdates = {
+        [researcher]: researcherData
+      };
       await db.runTransaction(async t => {
         let transactionWrites = [];
-         await assignExpPoints({
-          researcher,
+        await assignExpPoints({
+          researcher: { docId: researcher, ...researcherData },
           participant,
           session: "1st",
           project: "Annotating",
           checkRecallgrading: false,
-          eventId: "1ktl3t9aer52is8oh1b90nuv54",
+          eventId: "",
+          researchersUpdates,
           transactionWrites,
           t
         });
         await writeTransaction(transactionWrites, t);
       });
-      const researcherDoc = await db.collection("researchers").doc(researcher).get();
-      const researcherData = researcherDoc.data();
       expect(researcherData.projects["Annotating"].expPoints).toEqual(10);
     });
   });
