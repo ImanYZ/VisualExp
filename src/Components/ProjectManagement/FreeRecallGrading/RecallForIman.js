2872930e12ba2bb2b36641a48c53c27c36ae66af
Sam ouhra <ouhrac@gmail.com>
2023-09-26 02:34:29 +0100

    spacing

--- src/Components/ProjectManagement/FreeRecallGrading/RecallForIman.js
+++ src/Components/ProjectManagement/FreeRecallGrading/RecallForIman.js
@@ -405,7 +405,7 @@ const RecallForIman = props => {
             </Box> */}
           </Box>
           {"\n"}
-          <Box sx={{ mt: "5px" }}>OriginalPassage:</Box>
+          <Box sx={{ mt: "5px" }}>Original Passage:</Box>
           <Paper style={{ padding: "10px 19px 10px 19px", margin: "19px" }}>{currentBot.originalPassage}</Paper>
           <Box>Response :</Box>
           <Paper style={{ padding: "10px 19px 10px 19px", margin: "19px" }}>{currentBot.response}</Paper>
@@ -516,7 +516,7 @@ const RecallForIman = props => {
           <Typography variant="h5" component="h5">
             The Response has four grades, but do not satisfy the majority of votes :
           </Typography>
-          <Box sx={{ mt: "5px" }}>OriginalPassage:</Box>
+          <Box sx={{ mt: "5px" }}>Original Passage:</Box>
           <Paper style={{ padding: "10px 19px 10px 19px", margin: "19px" }}>{currentNoMajority.originalPassage}</Paper>
           <Box>Response :</Box>
           <Paper style={{ padding: "10px 19px 10px 19px", margin: "19px" }}>{currentNoMajority.response}</Paper>
