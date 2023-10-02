22aa170810d3e5787b3d707a3ec02acaf9e4e9bb
Sam ouhra <ouhrac@gmail.com>
2023-10-02 19:49:09 +0100

    fix issues updating phrases in the recall grades logs

--- functions/actions/researchers/updatePhraseForPassage.js
+++ functions/actions/researchers/updatePhraseForPassage.js
@@ -1,19 +1,28 @@
 const { db } = require("../../admin");
 
 const { dbReal } = require("../../admin_real");
-const { ObjectToArray, filterItemsByRubric, ArrayToObject } = require("../../helpers/grading-recalls");
+const { filterItemsByRubric, ArrayToObject } = require("../../helpers/grading-recalls");
 
 const replacePhraseInLogs = (array, oldphrase, newphrase) => {
   for (let i = 0; i < array.length; i++) {
     const item = array[i];
-    if (item.hasOwnProperty("rubric_item") && item.rubric_item === oldphrase) {
+    if (item.hasOwnProperty("rubric_item") && (item?.rubric_item || "").trim() === oldphrase) {
       item.rubric_item = newphrase;
     }
   }
 };
 
+const replaceLogs = ({ resetGradesGPT, conditionlogs, selectedPhrase, newPhrase }) => {
+  if (resetGradesGPT) {
+    conditionlogs = conditionlogs.map(subarray => filterItemsByRubric(subarray, [selectedPhrase]));
+  } else {
+    conditionlogs = conditionlogs.map(subarray => replacePhraseInLogs(subarray, selectedPhrase, newPhrase));
+  }
+};
+
 module.exports = async (req, res) => {
   try {
+    console.log("updatePhraseForPassage");
     const { passagTitle, selectedPhrase, newPhrase, resetGrades, resetGradesGPT } = req.body;
     const passageQuery = db.collection("passages").where("title", "==", passagTitle);
     const passageSnapshot = await passageQuery.get();
@@ -47,42 +56,39 @@ module.exports = async (req, res) => {
                 phraseItem.researchers = [];
                 phraseItem.grades = [];
               }
-              const recallGradesLogsRef = dbReal.ref(`/recallGradesGPTLogs/${recallDoc.id}`);
-              const previousLogDoc = await recallGradesLogsRef.once("value");
-              if (previousLogDoc.exists()) {
-                const previousLogData = previousLogDoc.val() || { sessions: {} };
-                const sessionItemLogs = previousLogData.sessions[session] || {};
-                const conditionItemLogs = sessionItemLogs[conditionItem.condition] || {};
-                if (conditionItemLogs.hasOwnProperty("gpt4")) {
-                  let consitionlogs_gpt4 = ObjectToArray(conditionItemLogs.gpt4);
-                  if (resetGradesGPT) {
-                    consitionlogs_gpt4 = consitionlogs_gpt4.map(subarray =>
-                      filterItemsByRubric(subarray, [selectedPhrase])
-                    );
-                    conditionItemLogs.gpt4 = ArrayToObject(consitionlogs_gpt4);
-                  } else {
-                    consitionlogs_gpt4 = consitionlogs_gpt4.map(subarray =>
-                      replacePhraseInLogs(subarray, selectedPhrase, newPhrase)
-                    );
-                  }
-                }
-                if (conditionItemLogs.hasOwnProperty("gpt35")) {
-                  let consitionlogs_gpt35 = ObjectToArray(conditionItemLogs.gpt35);
-                  if (resetGradesGPT) {
-                    consitionlogs_gpt35 = consitionlogs_gpt35.map(subarray =>
-                      filterItemsByRubric(subarray, [selectedPhrase])
-                    );
-                    conditionItemLogs.gpt35 = ArrayToObject(consitionlogs_gpt35);
-                  } else {
-                    consitionlogs_gpt35 = consitionlogs_gpt35.map(subarray =>
-                      replacePhraseInLogs(subarray, selectedPhrase, newPhrase)
-                    );
-                  }
-                }
-              }
             }
           }
         }
+      }
+
+      const recallGradesLogsRef = dbReal.ref(`/recallGradesGPTLogs/${recallDoc.id}`);
+      const previousLogDoc = await recallGradesLogsRef.once("value");
+      if (previousLogDoc.exists()) {
+        const previousLogData = previousLogDoc.val() || { sessions: {} };
+        console.log(previousLogData);
+        const sessionsLogs = previousLogData.sessions || {};
+        for (let session in sessionsLogs) {
+          for (let itemIdx in sessionsLogs[session]) {
+            const conditionItemLogs = sessionsLogs[session][itemIdx];
+            let conditionlogs_gpt4 = (conditionItemLogs?.gpt4 || []).filter(
+              item => item !== null && item !== undefined
+            );
+            let conditionlogs_gpt3_5 = (conditionItemLogs?.gpt35 || []).filter(
+              item => item !== null && item !== undefined
+            );
+            replaceLogs({ resetGradesGPT, conditionlogs: conditionlogs_gpt4, selectedPhrase, newPhrase });
+            replaceLogs({ resetGradesGPT, conditionlogs: conditionlogs_gpt3_5, selectedPhrase, newPhrase });
+
+            conditionItemLogs.gpt4 = ArrayToObject(conditionlogs_gpt4);
+            conditionItemLogs.gpt35 = ArrayToObject(conditionlogs_gpt3_5);
+          }
+        }
+        updateTasks.push(
+          recallGradesLogsRef.update({
+            updatedAt: new Date(),
+            ...previousLogData
+          })
+        );
       }
       if (needUpdate) {
         updateTasks.push(recallDoc.ref.update({ sessions }));
