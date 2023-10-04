28fd58e4878bf5c277a8a72915d978d8b4c08d44
Sam ouhra <ouhrac@gmail.com>
2023-10-04 22:01:42 +0100

    fix the cloudbuild

--- functions/admin_real.js
+++ functions/admin_real.js
@@ -5,22 +5,9 @@ require("dotenv").config({
   path: path.join(__dirname, "../", ".env")
 });
 
-const serviceAccount = {
-  type: process.env.VISUALEXPCRED_TYPE,
-  project_id: process.env.VISUALEXP_PROJECT_ID,
-  private_key_id: process.env.VISUALEXPCRED_PRIVATE_KEY_ID,
-  private_key: process.env.VISUALEXPCRED_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-  client_email: process.env.VISUALEXPCRED_CLIENT_EMAIL,
-  client_id: process.env.VISUALEXPCRED_CLIENT_ID,
-  auth_uri: process.env.VISUALEXPCRED_AUTH_URI,
-  token_uri: process.env.VISUALEXPCRED_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.VISUALEXPCRED_AUTH_PROVIDER_X509_CERT_URL,
-  client_x509_cert_url: process.env.VISUALEXPCRED_CLIENT_X509_CERT_URL
-};
-
 const app = admin.initializeApp(
   {
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.applicationDefault(),
     databaseURL: "https://visualexp-a7d2c-default-rtdb.firebaseio.com/"
   },
   "visualexp"
