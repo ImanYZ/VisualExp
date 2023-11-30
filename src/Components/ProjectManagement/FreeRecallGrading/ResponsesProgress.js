a0a5bee51df38c46dbda93eefdb3b1c0765f923a
Sam ouhra <ouhrac@gmail.com>
2023-11-30 20:26:35 +0100

    update the progress page

--- src/Components/ProjectManagement/FreeRecallGrading/ResponsesProgress.js
+++ src/Components/ProjectManagement/FreeRecallGrading/ResponsesProgress.js
@@ -2,13 +2,17 @@ import React, { useEffect, useState } from "react";
 import { useRecoilValue } from "recoil";
 import { firebaseState } from "../../../store/AuthAtoms";
 import Box from "@mui/material/Box";
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Typography } from "@mui/material";
 
 const ResponsesProgress = () => {
   const firebase = useRecoilValue(firebaseState);
   const [progress, setProgress] = useState({});
   useEffect(() => {
     const loadProgres = async () => {
-      const responsesProgressDocs = await firebase.db.collection("responsesProgress").orderBy("createdAt", "desc").get();
+      const responsesProgressDocs = await firebase.db
+        .collection("responsesProgress")
+        .orderBy("createdAt", "desc")
+        .get();
       const progressData = responsesProgressDocs.docs[0].data();
       console.log({ progressData });
       delete progressData.createdAt;
@@ -33,11 +37,25 @@ const ResponsesProgress = () => {
   return (
     <Box style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
       <Box>
-        {Object.keys(progress).map(p => (
-          <Box key={p} sx={{ mt: "9px" }}>
-            {p}: {progress[p]}
-          </Box>
-        ))}
+        <Typography sx={{ mb: "5px", fontSize: "20px" }}>Recall Grading Progress:</Typography>
+        <TableContainer component={Paper}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>The percentage of phrases done in a response</TableCell>
+                <TableCell>The number of those of responses</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {Object.keys(progress).map(p => (
+                <TableRow>
+                  <TableCell>{p}</TableCell>
+                  <TableCell>{progress[p]}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </Box>
     </Box>
   );
